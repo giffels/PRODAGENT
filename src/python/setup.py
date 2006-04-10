62976d11ad04057a758838dfa24e7930e3e1f325
evansde <>
2006-04-10 17:43:46 +0000

    add utils

---
+++ src/python/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+"""
+_ProdAgent_
+
+Python packages for the CMS Production Agent
+
+"""
+
+from distutils.core import setup
+
+
+packages = [
+    'CMSConfigTools',
+    'DBSInterface',
+    'DLSInterface',
+    'ErrorHandler',
+    'ErrorHandler.Handlers',
+    'FwkJobRep',
+    'IMProv',
+    'JobCreator',
+    'JobCreator.Creators',
+    'JobCreator.RuntimeTools',
+    'JobSubmitter',
+    'JobSubmitter.Submitters',
+    'JobTracking',
+    'MB',
+    'MB.commandBuilder',
+    'MB.dmb_tools',
+    'MB.creator',
+    'MB.query',
+    'MB.transport',
+    'MCPayloads',
+    'MergeSensor',
+    'MessageService',
+    'ProdAgentDB',
+    'ProdAgentCore',
+    'RequestInjector',
+    'RunRes',
+    'SeedGen',
+    'ShLogger',
+    'ShLogger.log_adapters',
+    'ShREEK',
+    'ShREEK.ControlPoints',
+    'ShREEK.ControlPoints.ActionImpl',
+    'ShREEK.ControlPoints.CondImpl',
+    'ShREEK.ShREEK_common',
+    'ShREEK.CMSPlugins',
+    'ShREEK.CMSPlugins.ApMon',
+    'ShREEK.CMSPlugins.JobMon',
+    ]
+
+setup(name='ProdAgent',
+      version='1.0',
+      description='CMS Production Agent',
+      author='Dave Evans',
+      author_email='evansde@fnal.gov',
+      url='https://twiki.cern.ch/twiki/bin/view/CMS/ProdAgent',
+      packages=packages,
+     )
