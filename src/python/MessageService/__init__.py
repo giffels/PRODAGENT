ec60910ef2fde5a92ca9f7938ee272c35337d9ca
evansde <>
2006-04-10 17:12:42 +0000

    Import MessageService package

---
+++ src/python/MessageService/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+_MesageServer
+
+Message interface
+"""
+
+__revision__ = "$Id: __init__.py,v 1.1 2006/03/08 11:42:56 ckavka Exp $"
+__version__ = "$Revision: 1.1 $"
+__author__ = "Carlos.Kavka@ts.infn.it"
+
+__all__ = []
+
+
+
+
