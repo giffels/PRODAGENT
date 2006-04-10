3cbb1f001c7e6ac2e71c99fc403a3d2349fe9036
evansde <>
2006-04-10 17:15:15 +0000

    Import ProdAgentDB package

---
+++ src/python/ProdAgentDB/Connect.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from ProdAgentDB.Config import defaultConfig
+from ProdAgentDB import Mysql
+
+def connect(**config):
+     """
+     _connect_  Connects to the underlying MySQL database.
+    
+     If a set of attributes is passed with the connect method,
+     it has to have 6 attributes:
+
+     dbName              : database name
+     host                : host (if local use "localhost")
+     user                : user name
+     passwd              : pass word
+     socketFileLocation  : socket file location (use if connect local)
+                           but leave empty if connecting from remote.
+     dbPortNr            : port number if you connect from remote.
+                           Leave empty if you connect via a socket file.
+     """
+     actualConfig = defaultConfig
+     if config != {}:
+          actualConfig = config
+     try:
+         conn=Mysql.connect(actualConfig['dbName'],\
+                            actualConfig['host'],\
+                            actualConfig['user'],\
+                            actualConfig['passwd'],\
+                            actualConfig['socketFileLocation'],\
+                            actualConfig['dbPortNr'])
+         return conn
+     except:
+         raise
+
---
+++ src/python/ProdAgentDB/Mysql.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+
+try:
+   import MySQLdb
+except:
+   # NOTE: we might need some mappings from error code number to 
+   # NOTE: error types?
+   raise RuntimeError(2,"MySQLdb could not be found. Make sure it is "+ \
+                            "installed or that the path is set correctly "+ \
+                            "more information at: " \
+                            "http://sourceforge.net/projects/mysql-python ")
+import time
+
+# Cache (connection pool) so we can reuse connections.
+__connectionCache={}
+# Refresh connections every 4 hours
+__refreshPeriod=4*3600
+# Check the connection every 3 minutes.
+__checkConnectionPeriod=3*60
+# Try to connect a maximum of 5 times.
+__maxConnectionAttempts=5
+# Time to wait to reconnect
+__dbWaitingTime=10
+       
+def connect(dbName,dbHost,dbUser,dbPasswd,socketLocation,portNr="",cache=True):
+
+   """
+   Generic connect method that returns a connection opbject.
+   We do not need to close this object as we can reuse it.
+   We do however have a refresh period to prevent the connection 
+   from actually "cutting" us of.
+   """
+   cacheKey=dbName+dbHost+dbUser+dbPasswd
+   if __connectionCache.has_key(cacheKey):
+       conn, staleness, lastCheck = __connectionCache[cacheKey]
+       timeDiff=time.time()-staleness
+       if(timeDiff > __refreshPeriod) or not cache:
+           conn.close()
+           del __connectionCache[cacheKey] 
+       else:
+           # test if the connection is still open?
+           # NOTE: is there a better way to do this?
+           # NOTE: checking the connection using a cursor
+           # NOTE: can be time consuming and 
+           # NOTE: defeats the use of a cache.
+           # NOTE: what I put in here is a hybrid solution.
+           timeDiff=time.time()-lastCheck
+           if(timeDiff > __checkConnectionPeriod):
+              try:
+                 cursor = conn.cursor()
+                 cursor.execute("SELECT CURDATE()")
+                 cursor.close()
+                 return conn
+              except:
+                 pass
+           else:
+              return conn
+   for attempt in range(__maxConnectionAttempts):
+       try:
+           if (portNr!=""):
+               conn=MySQLdb.Connect(unix_socket=socketLocation,\
+                                   host=dbHost,db=dbName,\
+                                   user=dbUser,passwd=dbPasswd, \
+                                   port=int(portNr))
+           else:
+               conn=MySQLdb.Connect(unix_socket=socketLocation,\
+                                   host=dbHost,db=dbName,\
+                                   user=dbUser,passwd=dbPasswd)
+           __connectionCache[cacheKey]=(conn,time.time(),time.time())
+           return conn
+       except Exception, v:
+           # wait and try again.
+           time.sleep(__dbWaitingTime)
+   raise RuntimeError(1,"Could not connect to database")
+       
