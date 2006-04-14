baacbbd5c12e597bd86f17a0006cf5654f6eac27
fvlingen <>
2006-04-14 07:26:57 +0000

    ErrorHandler_Message_t.py contains tests that publish messages to test the erorhandler only. It assumes the ErrorHandler component is running (no other components are running). This test is part of a suite.
    ReqInj_Message_t.py contains message publications (needs to be formatted
    using the testcase class) and assumes that the ReqInj component, JobCreator,
    JobSubmitter, ErrorHandler is running.  The messages in this file are for
    the ReqInj component which acts on them and generates messages for the
    JobCreator which generates messages for the JobSubmitter, etc..
    The payload of the messages are referenses to workflow spec files
    (these files are not part of CVS!).
    
    
    --------------------------------------
    Frank van Lingen
    California Institute of Technology
    CA 91125 Pasadena
    United States
    --------------------------------------
    Mail Code:356-48
    bld      :340 Lauritsen
    email    :fvlingen@caltech.edu
    im(aim)  :marcellus0872
    tel      :(+1) 626 395 3862
    mobile   :(+1) 310 968 5584
    url      :http://www.van-lingen.name/
    --------------------------------------

---
+++ test/python/IntTests/ReqInj_Message_t.py
@@ -0,0 +1,76 @@
+from MessageService.MessageService import MessageService
+import os
+import xmlrpclib
+
+print "Make sure the RequestInjector, JobCreator,"
+print "JobSubmitter and ErrorHandler are running"
+# we use this for event publication.
+ms=MessageService()
+ms.registerAs("TestComponent")
+# subscribe on the events this or other test produce
+# so we can verify this in the database
+
+
+# Request Injector generated
+#ms.subscribeTo("CreateJob")
+# Job Creator generated
+#ms.subscribeTo("SubmitJob")
+# Job Submitter generated
+#ms.subscribeTo("JobSubmitted")
+#ms.subscribeTo("SubmissionFailed")
+# Job Tracker generated
+ms.subscribeTo("JobFailed")
+ms.subscribeTo("JobSuccess")
+
+# Error Handler generated
+ms.subscribeTo("SubmissionFailed")
+ms.subscribeTo("JobSubmitted")
+ms.subscribeTo("GeneralJobFailure")
+
+# set the data directory.
+thisPath=os.getcwd()
+chunkIndex=thisPath.rfind('test/python')
+dataPath=thisPath[0:chunkIndex]+'test/data/input/'
+
+
+# publish events for first components. 
+ms.publish("RequestInjector:StartDebug","none")
+ms.publish("JobCreator:StartDebug","none")
+ms.publish("JobSubmitter:StartDebug","none")
+ms.publish("ErrorHandler:StartDebug","none")
+
+ms.publish("RequestInjector:SetWorkflow",dataPath+\
+                 'workflowSpec/PreProdR2Electron300GeV-Workflow.xml')
+ms.publish("RequestInjector:SetEventsPerJob",str(100))
+ms.publish("ResourcesAvailable", "none")
+
+ms.publish("RequestInjector:SetWorkflow",dataPath+\
+                'workflowSpec/PreProdR2Electron10GeV-Workflow.xml')
+ms.publish("RequestInjector:SetEventsPerJob",str(100))
+ms.publish("ResourcesAvailable", "none")
+
+ms.publish("RequestInjector:SetWorkflow",dataPath+\
+                 'workflowSpec/PreProdR2Mu10GeV-Workflow.xml')
+ms.publish("RequestInjector:SetEventsPerJob",str(100))
+ms.publish("ResourcesAvailable", "none")
+
+ms.publish("RequestInjector:SetWorkflow",dataPath+\
+                 'workflowSpec/PreProdR2Pion300GeV-Workflow.xml')
+ms.publish("RequestInjector:SetEventsPerJob",str(100))
+ms.publish("ResourcesAvailable", "none")
+
+ms.publish("RequestInjector:SetWorkflow",dataPath+\
+                 'workflowSpec/PreProdR2Mu300GeV-Workflow.xml')
+ms.publish("RequestInjector:SetEventsPerJob",str(100))
+ms.publish("ResourcesAvailable", "none")
+
+ms.publish("RequestInjector:SetWorkflow",dataPath+\
+                 'workflowSpec/PreProdR2Minbias-Workflow.xml')
+ms.publish("RequestInjector:SetEventsPerJob",str(100))
+ms.publish("ResourcesAvailable", "none")
+
+ms.publish("RequestInjector:SetWorkflow",dataPath+\
+                 'workflowSpec/PreProdR2Pion10GeV-Workflow.xml')
+ms.publish("RequestInjector:SetEventsPerJob",str(100))
+ms.publish("ResourcesAvailable", "none")
+ms.commit()
