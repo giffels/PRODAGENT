493a02d253c2da9957da807cccef3259c2ca726f
afanfani <>
2006-04-17 17:02:26 +0000

    test script which takes first run, number of events per jobs and create/submit jobs using lcg.

---
+++ test/python/IntTests/InjectTestLCG.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+"""
+Generate jobs for the workflow provided
+
+"""
+from MessageService.MessageService import MessageService
+import sys,os,getopt
+
+
+usage="\n Usage: python InjectTest.py <options> \n Options: \n --workflow=<workflow.xml> \t\t workflow file \n --run=<firstRun> \t\t first run number \n --nevts=<NumberofEvent> \t\t number of events per job \n --njobs=<NumberofEvent> \t\t number of jobs \n"
+
+valid = ['workflow=', 'run=', 'nevts=' , 'njobs=']
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "", valid)
+except getopt.GetoptError, ex:
+    print usage
+    print str(ex)
+    sys.exit(1)
+
+workflow = None
+run = None
+nevts = None
+njobs = None
+
+for opt, arg in opts:
+    if opt == "--workflow":
+        workflow = arg
+    if opt == "--run":
+        run = int(arg)
+    if opt == "--nevts":
+        nevts = int(arg)
+    if opt == "--njobs":
+        njobs = int(arg)
+
+
+if workflow == None:
+    print "--workflow option not provided"
+    print usage
+    sys.exit(1)
+if not os.path.exists(workflow):
+    print "Workflow not found: %s" % workflow
+    sys.exit(1)
+if run == None:
+    print "--run option not provided."
+    print usage
+    sys.exit(1)
+if nevts == None:
+    print "--nevts option not provided."
+    print usage
+    sys.exit(1)
+if njobs == None:
+    print "--njobs option not provided."
+    print usage
+    sys.exit(1)
+
+
+## use MessageService
+ms = MessageService()
+## register message service instance as "Test"
+ms.registerAs("Test")
+
+## Debug level
+ms.publish("RequestInjector:StartDebug","none")
+ms.publish("JobCreator:StartDebug","none")
+ms.publish("JobSubmitter:StartDebug","none")
+ms.publish("TrackingComponent:StartDebug","none")
+
+## Set Creator
+ms.publish("JobCreator:SetCreator","lcg")
+## Set Submitter
+ms.publish("JobSubmitter:SetSubmitter","lcg")
+## Set Workflow and NewDataset
+ms.publish("RequestInjector:SetWorkflow", workflow)
+ms.publish("RequestInjector:NewDataset",'')
+## Set first run and number of events per job
+ms.publish("RequestInjector:SetInitialRun", str(run))
+ms.publish("RequestInjector:SetEventsPerJob", str(nevts))
+## Loop over jobs
+njobs=njobs+1
+for i in range(1, njobs):
+ ms.publish("ResourcesAvailable","none")
+ ms.commit()
