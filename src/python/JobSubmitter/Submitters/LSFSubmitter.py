bee5f7d44c9585e40dc1f9f8d3435cd5c86d1707
bacchi <>
2006-04-26 18:31:37 +0000

    Support for BOSS 3 and BOSS 4

--- src/python/JobSubmitter/Submitters/LSFSubmitter.py
+++ src/python/JobSubmitter/Submitters/LSFSubmitter.py
@@ -48,25 +48,18 @@ class LSFSubmitter(SubmitterInterface):
             msg = "Error: BOSS environment BOSSVERSION not set:\n"
             raise RuntimeError, msg
 
+        self.BossVersion=os.environ["BOSSVERSION"].split('_')[0]
 
         # BOSS supported versions (best red from configration)
-        supportedBossVersions = ["v3_6_1","v3_6_2","v3_6_3","v4_0_0"]
+        supportedBossVersions = ["v3","v4"]
 
         # test if version is in supported versions list
-        if not supportedBossVersions.__contains__(os.environ["BOSSVERSION"]):
+        if not supportedBossVersions.__contains__(self.BossVersion):
             msg = "Error: BOSS version " +  os.environ["BOSSVERSION"] + " not supported:\n"
             msg += "supported versions are " + supportedBossVersions.__str__()
             raise RuntimeError, msg
-        
-        if os.environ["BOSSVERSION"]=="v4_0_0":
-            pass
-        else:
-            inf,outf=os.popen4("boss SQL -query \"select name from JOBTYPE where name = 'cmssw'\"")
-            outp=outf.read()
-            self.bossJobType="cmssw"
-            #print outp.find("cmssw")
-            if outp.find("cmssw")<0:
-                self.bossJobType="stdjob"
+        self.bossSubmitCommand={"v3":self.BOSS3submit,"v4":self.BOSS4submit}
+        self.parameters['Scheduler']="lsf"
 
 
     #  //
@@ -86,8 +79,8 @@ class LSFSubmitter(SubmitterInterface):
         script.append("./run.sh \n")
         script.append("cd ..\n")
         script.append("cp %s/FrameworkJobReport.xml . \n" % jobname)
-        script.append("cp %s/*/*.root .\n" % jobname )
-
+##         script.append("cp %s/*/*.root .\n" % jobname )
+        
         handle = open(wrapperName, 'w')
         handle.writelines(script)
         handle.close()
@@ -107,43 +100,18 @@ class LSFSubmitter(SubmitterInterface):
         Initial tests: No FrameworkJobReport yet, stage back stdout log
         
         """
-        #print "LXB1125Submitter.doSubmit: %s"  % wrapperScript
-        #print "LXB1125Submitter.doSubmit: %s"  % jobTarball
+        bossJobId=self.isBOSSDeclared()
+        if bossJobId==None:
+            self.declareToBOSS()
+            bossJobId=self.isBOSSDeclared()
+        print "bossJobId = %s"%bossJobId
+        JobName=self.parameters['JobName']
 
         #  //
         # // Build BOSS Declare command
         #//
-        #bossDeclare = "boss declare -jobtype %s " % bossJobType
-        bossDeclare = "boss declare -classad declare.clad"
-        declareClad=open("declare.clad","w")
-        declareClad.write("executable = %s;\n" % os.path.basename(wrapperScript))
-        declareClad.write("jobtype = %s;\n"%self.bossJobType)
-        declareClad.write("stdout = stdout.log;\n")
-        declareClad.write("stderr = stderr.log;\n")
-        declareClad.write("infiles = %s,%s;\n" % (wrapperScript, jobTarball))
-        #declareClad.write("outfiles = FrameworkJobReport.xml;\n")
-        declareClad.write("outfiles = *.root,stdout.log,stderr.log,FrameworkJobReport.xml;\n")
-#        declareClad.write("queue = 1nh;\n")
-        #%os.path.basename(wrapperScript).split("-submit")[0].strip())
-        declareClad.close()
-      
-        #bossDeclare += "-classad declare.clad"
-        #bossDeclare += "-infiles %s,%s " % (wrapperScript, jobTarball) 
+        bossSubmit = self.bossSubmitCommand[self.BossVersion](bossJobId)
 
-        #  //
-        # // Execute BOSS Declare command to get boss job id 
-        #//
-        
-       # print "LXB1125Submitter.doSubmit: %s" % bossDeclare
-        bossJobId = self.executeCommand(bossDeclare)
-       # print "-jobid %s " % bossJobId.split("Job ID")[1].strip()
-        #  //
-        # // Build BOSS Submit command using job id
-        #//
-        bossJobId = bossJobId.split("Job ID")[1].strip()
-        bossSubmit = "boss submit "
-        bossSubmit += "-jobid %s " % bossJobId
-        bossSubmit += "-scheduler %s " % bossScheduler
         #print "LXB1125Submitter.doSubmit: %s" % bossSubmit
         #  //
         # // Executing BOSS Submit command
@@ -152,14 +120,12 @@ class LSFSubmitter(SubmitterInterface):
         print "LSFSubmitter.doSubmit: %s" % output
         return
 
-# try:
-#     if os.access(compConfig.parameter("BOSSPATH")+"/boss",os.R_OK and os.X_OK):
-#         args["BOSSDIR"]=compConfig.parameter("BOSSDIR")
-#         args["BOSSVERSION"]=compConfig.parameter("BOSSVERSION")
-#         args["BOSSPATH"]=compConfig.parameter("BOSSPATH")
-#         os.environ["BOSSVERSION"]=args["BOSSVERSION"]
-# except StandardError, ex:
-#     pass
+
+
+
+
+
+
     
 
 registerSubmitter(LSFSubmitter, "lsf")
