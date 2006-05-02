ecda1b6f6dad674773586dc9f7905e7b4b418406
fvlingen <>
2006-05-02 21:26:26 +0000

    Added a test for the purgeStates() method.
    --------------------------------------
    Frank van Lingen
    California Institute of Technology
    CA 91125 Pasadena
    United States
    --------------------------------------
    Mail Code:356-48
    bld      :340 Lauritsen
    email    :fvlingen@caltech.edu
    im(aim)  :marcellus0872
    tel      :(+1) 626 395 3862
    mobile   :(+1) 310 968 5584
    url      :http://www.van-lingen.name/
    --------------------------------------

--- test/python/UnitTests/JobState_t2.py
+++ test/python/UnitTests/JobState_t2.py
@@ -18,18 +18,24 @@ class JobStateUnitTests2(unittest.TestCase):
     def testA(self):
         """change state test"""
         try:
-         for i in [1,2,3,4,5,6,7,8,9,10]:
-              try:
-                   JobStateChangeAPI.cleanout("jobClassID"+str(i))
-              except Exception,ex:
-                   self.assertEqual(ex[1],'This jobspec with ID jobClassID'+str(i)+' does not exist')
+          for i in [1,2]:
+              JobStateChangeAPI.cleanout("jobClassID"+str(i))
         except StandardError, ex:
             msg = "Failed State Change TestA:\n"
             msg += str(ex)
             self.fail(msg)
 
+    def testB(self):
+         try:
+            JobStateChangeAPI.purgeStates()
+         except StandardError, ex:
+            msg = "Failed State Change TestB:\n"
+            msg += str(ex)
+            self.fail(msg)
+
     def runTest(self):
         self.testA()
+        self.testB()
             
 if __name__ == '__main__':
     unittest.main()
