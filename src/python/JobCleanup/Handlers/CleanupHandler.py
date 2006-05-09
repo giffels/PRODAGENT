c4cab7d8564afd2d9ae58104b126c157096c8733
fvlingen <>
2006-05-09 07:21:17 +0000

    Updated the cleanup handler. Improved code to delete directories, and the cleanup handler now also purges the database entries associated to a specific job spec id for which the JobCleanup event is generated.
    ErrorHandlers are not responsible anymore for cleaning up. Instead they
    emit a JobCleanup event.
    
    
    --------------------------------------
    Frank van Lingen
    California Institute of Technology
    CA 91125 Pasadena
    United States
    --------------------------------------
    Mail Code:356-48
    bld      :340 Lauritsen
    email    :fvlingen@caltech.edu
    im(aim)  :marcellus0872
    tel      :(+1) 626 395 3862
    mobile   :(+1) 310 968 5584
    url      :http://www.van-lingen.name/
    --------------------------------------

--- src/python/JobCleanup/Handlers/CleanupHandler.py
+++ src/python/JobCleanup/Handlers/CleanupHandler.py
@@ -9,6 +9,7 @@ from JobCleanup.Registry import retrieveHandler
 
 from JobState.JobStateAPI import JobStateChangeAPI
 from JobState.JobStateAPI import JobStateInfoAPI
+from Trigger.TriggerAPI import TriggerAPI
 
 class CleanupHandler(HandlerInterface):
     """
@@ -24,14 +25,21 @@ class CleanupHandler(HandlerInterface):
          """
          The payload of a job failure is a url to the job report
          """
-         jobReportUrl= payload
 
          try:
              logging.debug(">CleanupHandler< removing cached files and state "+\
                             "information for jobspec: "+str(payload))
              cacheDirLocation=JobStateInfoAPI.general(str(payload))['CacheDirLocation']
              logging.debug(">CleanupHandler< removing directory: "+cacheDirLocation)
-             os.rmdir(cacheDirLocation)
+             try:
+                 for root, dirs, files in os.walk(cacheDirLocation, topdown=False):
+                     for name in files:
+                         os.remove(os.path.join(root, name))
+                     for name in dirs:
+                         os.rmdir(os.path.join(root, name))
+                 os.rmdir(cacheDirLocation)
+             except Exception,ex:
+                 logging.debug(">CleanupHandler< WARNING job cleanup: "+str(ex))
              JobStateChangeAPI.cleanout(str(payload))
          except Exception,ex:
              logging.debug(">CleanupHandler< ERROR job cleanup: "+str(ex))
