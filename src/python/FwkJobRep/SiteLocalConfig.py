7a73c495a87f5b1745a2c92b28c54b6706464c3b
evansde <>
2006-05-24 14:19:18 +0000

    Add prototype site local config reader that can be used by jobs to retrieve information from the site local config

---
+++ src/python/FwkJobRep/SiteLocalConfig.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+"""
+_SiteLocalConfig_
+
+Utility for reading a site local config XML file and converting it
+into an object with an API for getting info from it
+
+"""
+
+
+
+from IMProv.IMProvLoader import loadIMProvFile
+from IMProv.IMProvQuery import IMProvQuery
+
+
+class SiteConfigError(StandardError):
+    """
+    Exception class placeholder
+    """
+    pass
+
+
+class SiteLocalConfig:
+    """
+    _SiteLocalConfig_
+
+    Readonly API object for getting info out of the SiteLocalConfig file
+
+    """
+    def __init__(self, siteConfigXML):
+        self.siteConfigFile = siteConfigXML
+        self.siteName = None
+        self.eventData = {}
+        self.calibData = {}
+        self.read()
+
+
+    def read(self):
+        """
+        _read_
+
+        Load data from SiteLocal Config file and populate this object
+
+        """
+        try:
+            node = loadIMProvFile(self.siteConfigFile)
+        except StandardError, ex:
+            msg = "Unable to read SiteConfigFile: %s\n" % self.siteConfigFile
+            msg += str(ex)
+            raise SiteConfigError, msg
+
+        #  //
+        # // site name
+        #//
+        nameQ = IMProvQuery("/site-local-config/site")
+        nameNodes = nameQ(node)
+        if len(nameNodes) == 0:
+            msg = "Unable to find site name in SiteConfigFile:\n"
+            msg += self.siteConfigFile
+            raise SiteConfigError, msg
+        self.siteName = str(nameNodes[0].attrs.get("name"))
+
+        #  //
+        # // event data (Trivial Catalog location)
+        #//
+        
+        catalogQ = IMProvQuery("/site-local-config/site/event-data/catalog")
+        catNodes = catalogQ(node)
+        if len(catNodes) == 0:
+            msg = "Unable to find catalog entry for event data in:\n"
+            msg += self.siteConfigFile
+            raise SiteConfigError, msg
+
+        self.eventData['catalog'] = str(catNodes[0].attrs.get("url"))
+
+
+        #  //
+        # // calib data
+        #//
+        
+        calibQ = IMProvQuery("/site-local-config/site/calib-data/*")
+        
+        calibNodes = calibQ(node)
+        if len(calibNodes) == 0:
+            msg = "Unable to find calib data entry in:\n"
+            msg += self.siteConfigFile
+            raise SiteConfigError, msg
+        for calibNode in calibNodes:
+            self.calibData[str(calibNode.name)] = \
+                      str(calibNode.attrs.get("url"))
+
+        return
+
+
+
+if __name__ == '__main__':
+    siteConf = SiteLocalConfig("file:///home/evansde/work/PRODAGENT/src/python/FwkJobRep/testConfig.xml")
+
+    print siteConf.siteName
+    print siteConf.eventData
+    print siteConf.calibData
+    
