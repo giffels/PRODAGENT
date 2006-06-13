fc82b65a8107247a0cf8c1ec0b89796a75c920e6
bacchi <>
2006-06-13 10:22:32 +0000

    Added BOSSCondorGSubmitter registration.

--- src/python/JobSubmitter/Submitters/__init__.py
+++ src/python/JobSubmitter/Submitters/__init__.py
@@ -6,8 +6,8 @@ Submission interfaces package
 
 """
 __all__ = []
-__version__ = "$Revision: 1.1 $"
-__revision__ = "$Id: __init__.py,v 1.1 2006/04/10 19:49:02 evansde Exp $"
+__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: __init__.py,v 1.2 2006/06/01 16:46:08 evansde Exp $"
 
 
 
@@ -22,3 +22,4 @@ import LXB1125Submitter
 import LSFSubmitter
 import LCGSubmitter
 import OSGSubmitter
+import BOSSCondorGSubmitter
