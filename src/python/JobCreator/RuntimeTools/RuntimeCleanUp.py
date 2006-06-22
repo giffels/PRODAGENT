b58fa9b10e98a1d4a96c818bf1801a27bb65ff6e
evansde <>
2006-06-22 11:01:46 +0000

    Add skeleton implementation of clean up node type for removing files after the merge

---
+++ src/python/JobCreator/RuntimeTools/RuntimeCleanUp.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+_RuntimeCleanUp_
+
+Runtime binary file for CleanUp type nodes
+
+"""
+import sys
+
+
+
+
+if __name__ == '__main__':
+    print "RuntimeCleanUp invoked..."
+    sys.exit(0)
