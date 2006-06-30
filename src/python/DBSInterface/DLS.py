d96b77e5ed5742ae596ad0b5d4d2bc418de1ee9c
afanfani <>
2006-06-30 12:08:15 +0000

    add DLS module to query DLS for fileblock location and add location of new fileblock (the DLS type and server to use are defined in the DLSInterface block of PA configuration).

---
+++ src/python/DBSInterface/DLS.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+"""
+_DLS_
+
+Class to extract/insert info from DLS
+based on DLS API
+ 
+"""
+import os,string,exceptions
+
+from ProdAgentCore.Configuration import loadProdAgentConfiguration        
+import dlsClient
+from dlsDataObjects import *
+
+import logging
+# ##############
+class DLS:
+  """
+  _DLS_
+
+  interface to extract/insert info from DLS
+  """
+# ##############
+  def __init__(self, type, endpoint):
+    """
+     Construct api object.
+    """
+## get configuration here instead of doing it at DBSComponent startup
+#    try:
+#      config = loadProdAgentConfiguration()
+#      compCfg = config.getConfig("DLSInterface")
+#    except StandardError, ex:
+#      msg = "Error reading configuration:\n"
+#      msg += str(ex)
+#      raise RuntimeError, msg
+#
+#    type=compCfg['DLSType']
+#    endpoint=compCfg['DLSAddress']
+#
+    try:
+        self.dlsapi = dlsClient.getDlsApi(dls_type=type,dls_endpoint=endpoint)
+    except dlsApi.DlsApiError, inst:
+        msg = "Error when binding the DLS interface: " + str(inst)
+        raise RuntimeError, msg
+
+# #########################
+  def getFileBlockLocation(self,fileblock):
+        """
+         query DLS to get fileblock location
+        """
+        entryList=[]
+        try:
+         entryList=self.dlsapi.getLocations(fileblock)
+        except dlsApi.DlsApiError, inst:
+          msg = "Error in the DLS query: %s." % str(inst)
+          if "DLS Server don't respond" in msg:
+            raise RuntimeError, msg
+
+        SEList=[]
+        for entry in entryList:
+         for loc in entry.locations:
+          SEList.append(str(loc.host))
+
+        return SEList
+
+# ##############
+  def addEntryinDLS(self,fileblock,SE):
+        """
+          add fileblock associated to SE in DLS
+        """
+        fblock=DlsFileBlock(fileblock)
+        location=DlsLocation(SE)
+        entry=DlsEntry(fblock,[location])
+        try:
+          self.dlsapi.add([entry])
+        except dlsApi.DlsApiError, inst:
+          msg = "Error adding a DLS entry: %s." % str(inst)
+          print msg
+
+        return
+
