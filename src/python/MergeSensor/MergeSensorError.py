a8ab7b9e7dc904e53bd5bae702b8e86d919b4378
ckavka <>
2006-07-11 14:23:01 +0000

    * Added class InvalidDataTier exception.

--- src/python/MergeSensor/MergeSensorError.py
+++ src/python/MergeSensor/MergeSensorError.py
@@ -1,11 +1,19 @@
 
 class MergeSensorError(Exception):
-    """ Exception object for errors in MergeSensor component"""
+    """ Exception object for general errors in MergeSensor component"""
     
     def __init__(self, value):
         self.value = value
         
     def __str__(self):
         return repr(self.value)
+
+class InvalidDataTier(Exception):
+    """ Exception object for invalid data tier"""
     
-    
+    def __init__(self, value):
+        self.value = value
+        
+    def __str__(self):
+        return repr(self.value)
+        
