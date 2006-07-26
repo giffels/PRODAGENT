9dcb575e1f9b2d780e98069424469925c08ffda0
fvlingen <>
2006-07-26 20:28:50 +0000

    Added an extra try, except to make sure a db cursor is closed.
    --------------------------------------
    Frank van Lingen
    California Institute of Technology
    CA 91125 Pasadena
    United States
    --------------------------------------
    Mail Code:356-48
    bld      :340 Lauritsen
    email    :fvlingen@caltech.edu
    im(aim)  :marcellus0872
    tel      :(+1) 626 395 3862
    mobile   :(+1) 310 968 5584
    url      :http://www.van-lingen.name/
    --------------------------------------

--- src/python/Trigger/Database/Api/TriggerAPIMySQL.py
+++ src/python/Trigger/Database/Api/TriggerAPIMySQL.py
@@ -50,41 +50,55 @@ def resetFlag(triggerId,jobSpecId,flagId):
        raise
 
 def flagSet(triggerId,jobSpecId,flagId):
-   conn=connect()
-   dbCur=conn.cursor()
-   sqlStr="""SELECT FlagValue FROM tr_Trigger WHERE TriggerID="%s" 
-       AND FlagID="%s" AND JobSpecID="%s"; """ %(triggerId,flagId,jobSpecId)
-   dbCur.execute(sqlStr)
-   rows=dbCur.fetchall()
-   if (len(rows)==0):
-       return False
-   if rows[0][0]!="finished":
-       return False
-   return True
-
+   try:
+       conn=connect()
+       dbCur=conn.cursor()
+       sqlStr="""SELECT FlagValue FROM tr_Trigger WHERE TriggerID="%s" 
+           AND FlagID="%s" AND JobSpecID="%s"; """ %(triggerId,flagId,jobSpecId)
+       dbCur.execute(sqlStr)
+       rows=dbCur.fetchall()
+       if (len(rows)==0):
+           return False
+       if rows[0][0]!="finished":
+           return False
+       return True
+       dbCur.execute("COMMIT")
+       dbCur.close()
+   except:
+       dbCur.execute("ROLLBACK")
+       dbCur.close()
+       raise
+   
 def allFlagSet(triggerId,jobSpecId):
-   conn=connect()
-   dbCur=conn.cursor()
-   # check if all flags are set:
-   sqlStr=""" SELECT COUNT(*) FROM (SELECT COUNT(*) as total_count 
-       FROM tr_Trigger WHERE TriggerID="%s" AND JobSpecID="%s") as 
-       total_count, (SELECT COUNT(*) as total_count FROM tr_Trigger 
-       WHERE FlagValue="finished" AND TriggerID="%s" AND JobSpecID="%s") 
-       as finished_count WHERE 
-       total_count.total_count=finished_count.total_count; 
-       """ %(triggerId,jobSpecId,triggerId,jobSpecId)
-   dbCur.execute(sqlStr)
-   rows=dbCur.fetchall()
-   result=rows[0][0]
-   dbCur.close()
-   if(result==1):
-      return True
-   return False
+   try:
+       conn=connect()
+       dbCur=conn.cursor()
+       # check if all flags are set:
+       sqlStr=""" SELECT COUNT(*) FROM (SELECT COUNT(*) as total_count 
+           FROM tr_Trigger WHERE TriggerID="%s" AND JobSpecID="%s") as 
+           total_count, (SELECT COUNT(*) as total_count FROM tr_Trigger 
+           WHERE FlagValue="finished" AND TriggerID="%s" AND JobSpecID="%s") 
+           as finished_count WHERE 
+           total_count.total_count=finished_count.total_count; 
+           """ %(triggerId,jobSpecId,triggerId,jobSpecId)
+       dbCur.execute(sqlStr)
+       rows=dbCur.fetchall()
+       result=rows[0][0]
+       dbCur.close()
+       if(result==1):
+          return True
+       return False
+       dbCur.execute("COMMIT")
+       dbCur.close()
+   except:
+       dbCur.execute("ROLLBACK")
+       dbCur.close()
+       raise
 
 def addFlag(triggerId,jobSpecId,flagId):
-   conn=connect()
-   dbCur=conn.cursor()
    try:
+      conn=connect()
+      dbCur=conn.cursor()
       dbCur.execute("START TRANSACTION")
       sqlStr="""INSERT INTO tr_Trigger(JobSpecID,TriggerID,FlagID,FlagValue)
              VALUES("%s","%s","%s","start") ;""" %(jobSpecId,triggerId,flagId)
@@ -98,9 +112,9 @@ def addFlag(triggerId,jobSpecId,flagId):
           " already exists or jobspec ID does not exist.")
 
 def setAction(jobSpecId,triggerId,actionName):
-   conn=connect()
-   dbCur=conn.cursor()
    try:
+      conn=connect()
+      dbCur=conn.cursor()
       dbCur.execute("START TRANSACTION")
       sqlStr="""INSERT INTO tr_Action(jobSpecId,triggerId,actionName)
              VALUES("%s","%s","%s") ;""" %(jobSpecId,triggerId,actionName)
@@ -113,16 +127,22 @@ def setAction(jobSpecId,triggerId,actionName):
       raise ProdAgentException("Trigger "+triggerId+' does not exist')
 
 def getAction(triggerId,jobSpecId):
-   conn=connect()
-   dbCur=conn.cursor()
-   sqlStr="""SELECT ActionName FROM tr_Action WHERE TriggerID="%s" 
-          AND JobSpecID="%s" ; """ %(triggerId,jobSpecId)
-   dbCur.execute(sqlStr)
-   rows=dbCur.fetchall()
-   if(len(rows)==1):
-       return rows[0][0]
-   else:
-       raise ProdAgentException("No Action Associated")
+   try:
+       conn=connect()
+       dbCur=conn.cursor()
+       sqlStr="""SELECT ActionName FROM tr_Action WHERE TriggerID="%s" 
+              AND JobSpecID="%s" ; """ %(triggerId,jobSpecId)
+       dbCur.execute(sqlStr)
+       rows=dbCur.fetchall()
+       if(len(rows)==1):
+           return rows[0][0]
+       else:
+           raise ProdAgentException("No Action Associated")
+       dbCur.execute("COMMIT")
+       dbCur.close()
+   except:
+      dbCur.execute("ROLLBACK")
+      dbCur.close()
         
 
 def cleanout(jobSpecId):
