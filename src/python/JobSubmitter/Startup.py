6e18f906584c88ccde6edb1be40cff56001e98a2
evansde <>
2006-08-04 17:53:36 +0000

    Add post mortem wrapper to JobSubmitter Startup.py

--- src/python/JobSubmitter/Startup.py
+++ src/python/JobSubmitter/Startup.py
@@ -13,6 +13,7 @@ import getopt
 
 from ProdAgentCore.Configuration import loadProdAgentConfiguration
 from ProdAgentCore.CreateDaemon import createDaemon
+from ProdAgentCore.PostMortem import runWithPostMortem
 from JobSubmitter.JobSubmitterComponent import JobSubmitterComponent
 
 #  //
@@ -37,4 +38,5 @@ compCfg['ComponentDir'] = os.path.expandvars(compCfg['ComponentDir'])
 print "Starting JobSubmitter Component..."
 createDaemon(compCfg['ComponentDir'])
 component = JobSubmitterComponent(**dict(compCfg))
-component.startComponent()
+
+runWithPostMortem(component, compCfg['ComponentDir'])
