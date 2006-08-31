bbc377cfa1e6482b49bb788e77f30baac0a537f5
fvlingen <>
2006-08-31 22:27:59 +0000

    Extended the prodmgr interface test to verify retrieval functionality when a prod agent crashes.
    Frank.

--- test/python/UnitTests/ProdMgr_t.py
+++ test/python/UnitTests/ProdMgr_t.py
@@ -25,20 +25,79 @@ class ProdMgrUnitTests(unittest.TestCase):
 
         print('testA')
         result=ProdMgrAPI.userID(self.serverURL) 
+
+        print("Pretending that the client side is crashing!!")
+        retrievedResult=ProdMgrAPI.retrieve()
+        self.assertEqual(result,retrievedResult[0])
+        retrievedResult=ProdMgrAPI.retrieve(self.serverURL)
+        self.assertEqual(result,retrievedResult[0])
+        retrievedResult=ProdMgrAPI.retrieve(self.serverURL,'userID')
+        self.assertEqual(result,retrievedResult[0])
+        print("Recovered data lost in last service call")
+
         ProdMgrAPI.commit()
+
         print('ProdAgent ID is :'+str(result))
 
     def testB(self):
         print('testB')
         allocations=ProdMgrAPI.acquireAllocation(self.serverURL,self.request_id,15)
+
+        print("Pretending that the client side is crashing!!")
+        retrievedResult=ProdMgrAPI.retrieve()
+        self.assertEqual(allocations,retrievedResult[0])
+        retrievedResult=ProdMgrAPI.retrieve(self.serverURL)
+        self.assertEqual(allocations,retrievedResult[0])
+        retrievedResult=ProdMgrAPI.retrieve(self.serverURL,'acquireAllocation')
+        self.assertEqual(allocations,retrievedResult[0])
+        print("Recovered data lost in last service call")
+
         ProdMgrAPI.commit()
-        print('Acquired : '+str(len(allocations))+' locations: '+str(allocations))
+        print('Acquired : '+str(len(allocations))+' allocations: '+str(allocations))
+        print('Releasing: '+str(allocations[0]))
+        result=ProdMgrAPI.releaseAllocation(self.serverURL,allocations[0])
+
+        print("Pretending that the client side is crashing!!")
+        retrievedResult=ProdMgrAPI.retrieve()
+        self.assertEqual(result,retrievedResult[0])
+        retrievedResult=ProdMgrAPI.retrieve(self.serverURL)
+        self.assertEqual(result,retrievedResult[0])
+        retrievedResult=ProdMgrAPI.retrieve(self.serverURL,'releaseAllocation')
+        self.assertEqual(result,retrievedResult[0])
+        print("Recovered data lost in last service call")
+
+        ProdMgrAPI.commit()
+        new_allocation=ProdMgrAPI.acquireAllocation(self.serverURL,self.request_id,1)
+
+        print("Pretending that the client side is crashing!!")
+        retrievedResult=ProdMgrAPI.retrieve()
+        self.assertEqual(new_allocation,retrievedResult[0])
+        retrievedResult=ProdMgrAPI.retrieve(self.serverURL)
+        self.assertEqual(new_allocation,retrievedResult[0])
+        retrievedResult=ProdMgrAPI.retrieve(self.serverURL,'acquireAllocation')
+        self.assertEqual(new_allocation,retrievedResult[0])
+        print("Recovered data lost in last service call")
+
+        ProdMgrAPI.commit()
+        print('Acquired : '+str(new_allocation))
+
+        
 
     def testC(self):
         print('testC')
         parameters={'numberOfJobs':20,
                     'prefix':'Wave1'}
         jobs=ProdMgrAPI.acquireJob(self.serverURL,self.request_id,parameters)
+
+        print("Pretending that the client side is crashing!!")
+        retrievedResult=ProdMgrAPI.retrieve()
+        self.assertEqual(jobs,retrievedResult[0])
+        retrievedResult=ProdMgrAPI.retrieve(self.serverURL)
+        self.assertEqual(jobs,retrievedResult[0])
+        retrievedResult=ProdMgrAPI.retrieve(self.serverURL,'acquireJob')
+        self.assertEqual(jobs,retrievedResult[0])
+        print("Recovered data lost in last service call")
+
         ProdMgrAPI.commit()
         ProdMgrUnitTests.jobs=jobs
         print('Acquired : '+str(len(jobs))+' jobs: '+str(jobs))
@@ -50,6 +109,16 @@ class ProdMgrUnitTests(unittest.TestCase):
         for job_index in xrange(0,len(jobs)-5):
             jobspec=jobs[job_index]['jobSpecId']
             finished=ProdMgrAPI.releaseJob(self.serverURL,str(jobspec),30)
+
+            print("Pretending that the client side is crashing!!")
+            retrievedResult=ProdMgrAPI.retrieve()
+            self.assertEqual(finished,retrievedResult[0])
+            retrievedResult=ProdMgrAPI.retrieve(self.serverURL)
+            self.assertEqual(finished,retrievedResult[0])
+            retrievedResult=ProdMgrAPI.retrieve(self.serverURL,'releaseJob')
+            self.assertEqual(finished,retrievedResult[0])
+            print("Recovered data lost in last service call")
+
             ProdMgrAPI.commit()
 
     def testE(self):
@@ -58,6 +127,16 @@ class ProdMgrUnitTests(unittest.TestCase):
         parameters={'numberOfJobs':20,
                     'prefix':'Wave2'}
         jobs=ProdMgrAPI.acquireJob(self.serverURL,self.request_id,parameters)
+
+        print("Pretending that the client side is crashing!!")
+        retrievedResult=ProdMgrAPI.retrieve()
+        self.assertEqual(jobs,retrievedResult[0])
+        retrievedResult=ProdMgrAPI.retrieve(self.serverURL)
+        self.assertEqual(jobs,retrievedResult[0])
+        retrievedResult=ProdMgrAPI.retrieve(self.serverURL,'acquireJob')
+        self.assertEqual(jobs,retrievedResult[0])
+        print("Recovered data lost in last service call")
+
         ProdMgrAPI.commit()
         print('Acquired : '+str(len(jobs))+' jobs: '+str(jobs))
         ProdMgrUnitTests.jobs+=jobs
@@ -70,6 +149,16 @@ class ProdMgrUnitTests(unittest.TestCase):
            events_completed=jobs[job_index]['end_event']-jobs[job_index]['start_event']+1
            try:
               finished=ProdMgrAPI.releaseJob(self.serverURL,str(jobspec),events_completed)
+
+              print("Pretending that the client side is crashing!!")
+              retrievedResult=ProdMgrAPI.retrieve()
+              self.assertEqual(finished,retrievedResult[0])
+              retrievedResult=ProdMgrAPI.retrieve(self.serverURL)
+              self.assertEqual(finished,retrievedResult[0])
+              retrievedResult=ProdMgrAPI.retrieve(self.serverURL,'releaseJob')
+              self.assertEqual(finished,retrievedResult[0])
+              print("Recovered data lost in last service call")
+
               ProdMgrAPI.commit()
               if type(finished)==bool:
                  if finished:
