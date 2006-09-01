6f7236640a31b7abfe00b4d8f7a6a772d5af1e42
ckavka <>
2006-09-01 13:30:18 +0000

    * Added MergeSensor status information table.

--- src/sql/ProdAgentDB/update-prodagentdb-to_0_0_42.sql
+++ src/sql/ProdAgentDB/update-prodagentdb-to_0_0_42.sql
@@ -27,6 +27,30 @@ CREATE TABLE ws_last_call
 
   ) Type=InnoDB;
 
+/*
+A merge_control table stores information on the status of the
+MergeSensor component.
+
+Fields:
+
+ id            internal id
+ status        running or stopped
+ mergedjobs    number of merge jobs generated so far
+ limited       is there a limit on jobs generation? (yes or no)
+ remainingjobs number of jobs that still can be generated
+*/
+
+CREATE TABLE merge_control
+  (
+    id int NOT NULL auto_increment,
+    status enum("running", "stopped") default "running",
+    mergedjobs int NOT NULL default '0',
+    limited enum("no", "yes") default "no",
+    remainingjobs int NOT NULL default '0',
+
+    PRIMARY KEY(id)
+  )
+ TYPE = InnoDB DEFAULT CHARSET=latin1;
 
 
 
