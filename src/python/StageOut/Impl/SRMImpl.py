68f2e00c1475c4f70c36d8db7cc01e4fae807032
evansde <>
2006-09-11 19:47:51 +0000

    Add honking great hack around srmcp command to check file size is non-zero and the SE and local file sizes are identical.

--- src/python/StageOut/Impl/SRMImpl.py
+++ src/python/StageOut/Impl/SRMImpl.py
@@ -5,9 +5,12 @@ _SRMImpl_
 Implementation of StageOutImpl interface for SRM
 
 """
-
+import os
 from StageOut.Registry import registerStageOutImpl
 from StageOut.StageOutImpl import StageOutImpl
+
+
+
 
 
 
@@ -39,7 +42,35 @@ class SRMImpl(StageOutImpl):
         if options != None:
             result += " %s " % options
         result += " %s " % sourcePFN
-        result += " %s " % targetPFN
+        result += " %s \n" % targetPFN
+
+        fileBaseName = os.path.basename(sourcePFN)
+
+        result += "FILE_SIZE=`stat -c %s "
+        result += " %s`\n" % fileBaseName
+        result += "echo \"Local File Size is: $FILE_SIZE\"\n"
+        metadataCheck = \
+        """
+        for ((a=1; a <= 10 ; a++))
+        do
+           SRM_SIZE=`srm-get-metadata %s 2>/dev/null | grep 'size :[0-9]' | cut -f2 -d":"`
+           echo "SRM Size is $SRM_SIZE"
+           if (( $SRM_SIZE > 0 )); then
+              if (( $SRM_SIZE == $FILE_SIZE )); then
+                 exit 0
+              else
+                 echo "Error: Size Mismatch between local and SE"
+                 exit 60311
+              fi 
+           else
+              sleep 2
+           fi
+        done
+        exit 60311
+
+        """ % targetPFN
+        result += metadataCheck
+        
         return result
 
     
