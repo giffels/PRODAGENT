e5d28f71219378f9b7e5485ae1a97c36a20d7962
evansde <>
2006-09-13 18:13:11 +0000

    Bug fix to escape the error description string to avoid special characters causing problems with MySQL insert

--- src/python/StatTracker/StatTrackerDB.py
+++ src/python/StatTracker/StatTrackerDB.py
@@ -171,7 +171,7 @@ def insertJobFailure(jobFailureInstance):
         jobFailureInstance['error_type'],
         jobFailureInstance['error_code'],
         jobFailureInstance['exit_code'],
-        jobFailureInstance['error_desc'],
+        MySQLdb.escape_string(jobFailureInstance['error_desc']),
         )
     connection = connect()
     dbCur = connection.cursor()
