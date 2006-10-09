c947c6b0bd5493c21f460cccded759a65b635112
fvlingen <>
2006-10-09 05:52:43 +0000

    Added tables for a persistent request priority queue and additional tables for managing (robust) interaction with the prodmgr
    Frank.

--- src/sql/ProdAgentDB/update-prodagentdb-to_0_0_44.sql
+++ src/sql/ProdAgentDB/update-prodagentdb-to_0_0_44.sql
@@ -2,3 +2,67 @@
 
 
 ALTER TABLE di_job_queue ADD status ENUM ("new", "used") DEFAULT "new";
+
+/*
+ * ======================Start Robust Service call tables===============
+ */
+
+CREATE TABLE ws_queue
+   (
+    component_id          varchar(150)    not null,
+    handler_id            varchar(150)    not null,
+    server_url            varchar(150)    not null,
+    state                 varchar(150)    not null,
+    parameters            mediumtext      not null,
+    log_time timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
+   ) Type=InnoDB;
+
+
+/*
+ * ======================End Robust Service call tables===============
+ */
+
+/*
+ * ======================Start PA interface tables===============
+ */
+
+CREATE TABLE pm_state
+   (
+    id                    varchar(150)    not null,
+    state                 varchar(150)    not null,
+    parameters            mediumtext      not null,
+    primary key(id)
+   ) Type=InnoDB;
+
+CREATE TABLE pm_request
+   (
+    id                    varchar(150)    not null,
+    url                   varchar(150)    not null,
+    priority              int(11)         not null
+   ) Type=InnoDB;
+
+CREATE TABLE pm_allocation
+   (
+    id                    varchar(150)    not null,
+    request_id            varchar(150)    not null,
+    catagory              varchar(150)    not null,
+    state                 varchar(150)    not null
+   ) Type=InnoDB;
+
+CREATE TABLE pm_job
+   (
+    id                    varchar(150)    not null,
+    request_id            varchar(150)    not null,
+    url                   varchar(150)    not null,
+    downloaded            int(11)         not null default 0,
+    catagory              varchar(150)    not null
+   ) Type=InnoDB;
+
+
+
+/*
+ * ======================End PA interface tables===============
+ */
+
+
+
