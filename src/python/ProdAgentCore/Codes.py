75f0857cc76c9af09e8b2c18b48f20a1557536f5
fvlingen <>
2006-10-18 17:40:03 +0000

    Exception list (late commit)
    Frank.

---
+++ src/python/ProdAgentCore/Codes.py
@@ -0,0 +1,9 @@
+
+# this will be a dictionary of error/exceptions codes
+
+errors={3000:'No result in local last service call table with a certain component',\
+        3001:'General database error',\
+        3002:'No connection when trying to start transaction',\
+        3003:'No transaction has started when trying to get a cursor',\
+        3004:'Unknown error',\
+       }
