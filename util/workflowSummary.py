eea864d6aa6954ffc17a21b2783c50f2f73f96dd
evansde <>
2006-11-17 22:50:01 +0000

    Add --timing option to workflowSummary to generate timing info for processing jobs

--- util/workflowSummary.py
+++ util/workflowSummary.py
@@ -10,7 +10,7 @@ Extract details from StatTracker DB and produce a summary for a given workflow
 import sys
 import getopt
 
-valid = ['workflows-matching=', 'interval=', 'workflow=']
+valid = ['workflows-matching=', 'interval=', 'workflow=', 'timing']
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], "", valid)
@@ -21,6 +21,7 @@ except getopt.GetoptError, ex:
 workflowMatch = None
 workflowName = None
 interval = "24:00:00"
+doTiming = False
 
 
 for opt, arg in opts:
@@ -30,7 +31,9 @@ for opt, arg in opts:
         interval = arg
     if opt == "--workflow":
         workflowName = arg
-
+    if opt == "--timing":
+        doTiming = True
+        
 
 if workflowMatch == None:
     workflowMatch = ""
@@ -56,6 +59,56 @@ for wf in allWorkflows:
         print Stats.shortTextWorkflowSummary(wf)
 
 
+        procSuccess = Stats.processingSuccessDetails(wf, interval)
+        mergeSuccess = Stats.mergeSuccessDetails(wf, interval)
+
+        procProps = {}
+        mergeProps = {}
+        
+        for procJob in procSuccess:
+            procProps[procJob['job_spec_id']] = procJob
+            procProps[procJob['job_spec_id']]['attrs'] = \
+               Stats.successfulJobProperties(procJob['job_index'])
+
+        for mergeJob in mergeSuccess:
+            mergeProps[mergeJob['job_spec_id']] = mergeJob
+            mergeProps[mergeJob['job_spec_id']]['attrs'] = \
+               Stats.successfulJobProperties(mergeJob['job_index'])
+        
+            
+        if doTiming:
+            procTimes = []
+            mergeTimes = []
+            
+            for props in procProps.values():
+                events = props['events_written']
+                appStart = props['attrs']['timing']['AppStartTime']
+                appEnd = props['attrs']['timing']['AppEndTime']
+                timeTaken = int(appEnd) - int(appStart)
+                timePerEvent = float(timeTaken) / float(events)
+                procTimes.append(int(timePerEvent))
+
+
+            avgProcTime = 0
+            maxProcTime = 0
+            minProcTime = 0
+            if len(procTimes) > 0:
+                avgProcTime = float(sum(procTimes)) / float (len(procTimes))
+            maxProcTime = max(procTimes)
+            minProcTime =  min(procTimes)
+
+            msg = "Processing Times (secs):\tAvg Time/Event: "
+            msg += "%s " % avgProcTime
+            msg += "\tMin: %s  \tMax: %s \n" % (
+                minProcTime, maxProcTime )
+            
+
+            
+            print msg
+            
+            
+            
+        
 
 
 #
