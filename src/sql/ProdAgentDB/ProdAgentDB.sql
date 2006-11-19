7f1e9a84fc39aa347845762b2c69a374fafa98b9
fvlingen <>
2006-11-19 20:31:44 +0000

    Removed a table used by the prodmgrinterface (not used by other components)
    Frank.

--- src/sql/ProdAgentDB/ProdAgentDB.sql
+++ src/sql/ProdAgentDB/ProdAgentDB.sql
@@ -593,15 +593,8 @@ CREATE TABLE pm_request
    (
     id                    varchar(150)    not null,
     url                   varchar(150)    not null,
-    priority              int(11)         not null
-   ) Type=InnoDB;
-
-CREATE TABLE pm_allocation
-   (
-    id                    varchar(150)    not null,
-    request_id            varchar(150)    not null,
-    catagory              varchar(150)    not null,
-    state                 varchar(150)    not null
+    priority              int(11)         not null,
+    primary key(id,url)
    ) Type=InnoDB;
 
 CREATE TABLE pm_job
@@ -609,6 +602,7 @@ CREATE TABLE pm_job
     id                    varchar(150)    not null,
     request_id            varchar(150)    not null,
     url                   varchar(150)    not null,
+    server_url            varchar(150)    not null,
     downloaded            int(11)         not null default 0,
     catagory              varchar(150)    not null
    ) Type=InnoDB;
@@ -618,7 +612,7 @@ CREATE TABLE pm_cooloff
     url                   varchar(150)    not null,
     `delay`               varchar(50) NOT NULL default '00:00:00',
     log_time timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-    index(url)
+    primary key(url)
    ) Type=InnoDB;
 
 /*
