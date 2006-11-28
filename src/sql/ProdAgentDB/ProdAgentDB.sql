3589f2220d8b73ea42e00484b96a9e47d3ed1e31
evansde <>
2006-11-28 22:09:27 +0000

    Include DB backup for CondorTracker

--- src/sql/ProdAgentDB/ProdAgentDB.sql
+++ src/sql/ProdAgentDB/ProdAgentDB.sql
@@ -644,3 +644,31 @@ CREATE TABLE jq_queue(
 /*
  * ======================End JobQueue tables===============
  */
+
+/*
+ * ======================End CondorTracker tables===============
+ */
+
+CREATE TABLE ct_job(
+  job_index INT NOT NULL AUTO_INCREMENT,
+  job_spec_id VARCHAR(255) NOT NULL,
+  job_state ENUM("submitted", "running", "killed", "complete") DEFAULT "submitted",
+  time timestamp NOT NULL default CURRENT_TIMESTAMP
+     ON UPDATE CURRENT_TIMESTAMP,	
+  UNIQUE (job_spec_id),
+  PRIMARY KEY(job_index),
+  INDEX(job_spec_id)
+  
+) TYPE=InnoDB;
+
+
+CREATE TABLE ct_job_attr(
+   job_index INT,
+   attr_index INT NOT NULL AUTO_INCREMENT,
+   attr_name VARCHAR(255),
+   attr_value BLOB,
+   PRIMARY KEY (attr_index),
+   FOREIGN KEY (job_index) REFERENCES ct_job(job_index)
+    ON DELETE CASCADE
+
+) TYPE=InnoDB;
