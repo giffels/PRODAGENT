b5cd42ced2c13e87355e4440cc9a0ea089ac2e43
evansde <>
2006-11-29 19:26:25 +0000

    Tweak CondorTracker table

--- src/sql/ProdAgentDB/ProdAgentDB.sql
+++ src/sql/ProdAgentDB/ProdAgentDB.sql
@@ -652,7 +652,7 @@ CREATE TABLE jq_queue(
 CREATE TABLE ct_job(
   job_index INT NOT NULL AUTO_INCREMENT,
   job_spec_id VARCHAR(255) NOT NULL,
-  job_state ENUM("submitted", "running", "killed", "complete") DEFAULT "submitted",
+  job_state ENUM("submitted", "running", "killed", "complete", "failed") DEFAULT "submitted",
   time timestamp NOT NULL default CURRENT_TIMESTAMP
      ON UPDATE CURRENT_TIMESTAMP,	
   UNIQUE (job_spec_id),
