60023e2623c3ed643d78c2910017b80b8bdabd89
afanfani <>
2007-01-28 17:48:45 +0000

    remove lsf and lxb1125 plugins (got tired of the warnings they cause at startup)

--- src/python/JobCreator/Creators/__init__.py
+++ src/python/JobCreator/Creators/__init__.py
@@ -5,8 +5,8 @@ _JobCreator.Creators_
 Plugins and tools for creating Jobs and acting on TaskObjects
 
 """
-__version__ = "$Revision: 1.3 $"
-__revision__ = "$Id: __init__.py,v 1.3 2006/05/31 20:06:08 evansde Exp $"
+__version__ = "$Revision: 1.4 $"
+__revision__ = "$Id: __init__.py,v 1.4 2007/01/12 23:20:31 evansde Exp $"
 __all__ = []
 
 
@@ -14,10 +14,10 @@ __all__ = []
 # // import modules containing creators here to automatically register them
 #//
 import TestCreator
-import LXB1125Creator
+#import LXB1125Creator
 import LCGCreator
 import FNALCreator
-import LSFCreator
+#import LSFCreator
 import LCGCreator
 import ExampleCreator
 import OSGCreator
