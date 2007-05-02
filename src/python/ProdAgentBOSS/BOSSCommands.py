a57394781f069972bacd92d147e382196278eb13
afanfani <>
2007-05-02 23:02:21 +0000

    temporary go back to PA021 version

--- src/python/ProdAgentBOSS/BOSSCommands.py
+++ src/python/ProdAgentBOSS/BOSSCommands.py
@@ -12,7 +12,6 @@ from JobState.JobStateAPI import JobStateInfoAPI
 import shutil
 from ProdAgentCore.ProdAgentException import ProdAgentException
 
-
 def checkSuccess(id,bossCfgDir):
     success=False
     try:
@@ -44,9 +43,6 @@ def checkSuccess(id,bossCfgDir):
         
     success=(outp=="0")
     return success
-
-
-
 def checkCrabSuccess(id,bossCfgDir):
     #print "CRAB"
     success=False
@@ -86,32 +82,17 @@ def resubmit(bossJobId,bossCfgDir):
     bossSubmit += " -c " + bossCfgDir + " "
     return bossSubmit
 
-
 def submit(bossJobId,scheduler,bossCfgDir):
     """
     BOSSsubmit
     
     BOSS command to submit a task
     """
-    
     bossSubmit = "boss submit "
-
-    ids = bossJobId.split(".")
-    taskid=""
-    try:
-        taskid = ids[0]
-    except:
-        raise ProdAgentException("Missing BOSS taskid")
-    try:
-        bossSubmit += "-jobid %s " % ids[1]
-    except:
-        pass
-    
-    bossSubmit += "-taskid %s " % taskid
+    bossSubmit += "-taskid %s " % bossJobId
     bossSubmit += "-scheduler %s " %  scheduler
     bossSubmit += " -c " + bossCfgDir + " "
     return bossSubmit
-
 
 def getTaskIdFromName(taskName,bossCfgDir):
     """
@@ -125,70 +106,6 @@ def getTaskIdFromName(taskName,bossCfgDir):
     except:
         return 0
     return outp
-
-
-def chainTemplate(parameters, bossJobType):
-    """
-    BOSS4createXML
-
-    BOSS 4 command to declare a task
-    """
-
-    chain = "   <chain name=\"%s\">\n" % parameters['JobName']
-    chain += "      <program>\n"
-    chain += "         <exec>          <![CDATA[ %s ]]> </exec>\n" \
-                      % os.path.basename(parameters['Wrapper'] )
-    chain += "         <args>          <![CDATA[ %s ]]> </args>\n" \
-                      % parameters['JobName']
-    chain += "         <stdin>         <![CDATA[ "" ]]> </stdin>\n"
-    chain += "         <stdout>        <![CDATA[ %s.stdout ]]> </stdout>\n" \
-                      % parameters['JobName']
-    chain += "         <stderr>        <![CDATA[ %s.stderr ]]> </stderr>\n" \
-                      % parameters['JobName']
-    chain += "         <program_types> <![CDATA[ %s ]]> </program_types>\n" \
-                      % bossJobType
-    chain += "         <infiles>       <![CDATA[ %s,%s ]]> </infiles>\n" \
-                      % (parameters['Wrapper'],parameters['Tarball'] )
-    chain += "         <outfiles>      <![CDATA[ *.root,%s.stdout,%s.stderr,FrameworkJobReport.xml ]]> </outfiles>\n" \
-                      % (parameters['JobName'], parameters['JobName'])
-    chain += "      </program>\n"
-    chain += "   </chain>\n"
-    
-#    logging.debug("SubmitterInterface:BOSS xml declare file written:%s" % xmlfile)
-    return chain
-
-
-def getIdFromJobName(bossCfgDir,JobName):
-    """
-    _getIdFromJobName___
-    
-    If this job has been declared to BOSS, return the BOSS ID
-    from the cache area. If it has not, return None
-    
-    """
-
-    query = \
-           "bossAdmin SQL -query \"select TASK_ID,ID from CHAIN where NAME='" + JobName + "'\" -c " + bossCfgDir
-#    logging.debug(query)
-    out = executeCommand(query)
-#    logging.debug(out)
-    outf = out.strip().split("\n")
-    try:
-        if outf[0].find( "No results!" ) >=0 :
-            jobId = ""
-        elif len( outf ) == 2 :
-            jobId = outf[1].strip()
-            jobId = jobId[0:jobId.find(' ')] + '.' + jobId[jobId.rfind(' ')+1:]
-        elif len( outf ) > 1 :
-            logging.debug(outf)
-            raise  (ProdAgentException( "job declared %d times" % len( outf ) ))
-        else:
-            outf = outf[1].split()
-            jobId = "%s.%s"% (outf[0], outf[1] )
-    except ValueError:
-        jobId = ""
-
-    return jobId
 
 
 def declare(bossCfgDir,parameters):
@@ -219,21 +136,10 @@ def declare(bossCfgDir,parameters):
     declareClad=open(xmlfile,"w")
     declareClad.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n")
 
-
-###############################################################################
-# OLD
-#    declareClad.write("<task name=\"%s\">"%parameters['JobName'])
-#    declareClad.write("<chain scheduler=\"%s\" ch_tool_name=\"\">"%parameters['Scheduler'])
-#    # declareClad.write(" <program exec=\"%s\" args=\"\" stderr=\"%s.stderr\" program_types=\"%s\" stdin=\"\" stdout=\"%s.stdout\"  infiles=\"%s,%s\" outfiles=\"*.root,%s.stdout,%s.stderr,FrameworkJobReport.xml\"  outtopdir=\"\"/></chain></task>"% (os.path.basename(self.parameters['Wrapper']), self.parameters['JobName'],bossJobType, self.parameters['JobName'],self.parameters['Wrapper'],self.parameters['Tarball'], self.parameters['JobName'], self.parameters['JobName']))
-#    declareClad.write(" <program> <exec><![CDATA[%s]]></exec><args><![CDATA[""]]></args><stderr><![CDATA[%s.stderr]]></stderr><program_types><![CDATA[%s]]></program_types><stdin><![CDATA[""]]></stdin><stdout><![CDATA[%s.stdout]]></stdout><infiles><![CDATA[%s,%s]]></infiles><outfiles><![CDATA[*.root,%s.stdout,%s.stderr,FrameworkJobReport.xml]]></outfiles><outtopdir><![CDATA[""]]></outtopdir></program></chain></task>"% (os.path.basename(parameters['Wrapper']), parameters['JobName'],bossJobType, parameters['JobName'],parameters['Wrapper'],parameters['Tarball'], parameters['JobName'], parameters['JobName']))
-###############################################################################
-# NEW
-    declareClad.write("<task name=\"%s\">\n" \
-                      % parameters['JobSpecInstance'].payload.workflow )
-    declareClad.write( chainTemplate(parameters, bossJobType) )
-    declareClad.write("</task>\n")
-###############################################################################
-    
+    declareClad.write("<task name=\"%s\">"%parameters['JobName'])
+    declareClad.write("<chain scheduler=\"%s\" ch_tool_name=\"\">"%parameters['Scheduler'])
+    # declareClad.write(" <program exec=\"%s\" args=\"\" stderr=\"%s.stderr\" program_types=\"%s\" stdin=\"\" stdout=\"%s.stdout\"  infiles=\"%s,%s\" outfiles=\"*.root,%s.stdout,%s.stderr,FrameworkJobReport.xml\"  outtopdir=\"\"/></chain></task>"% (os.path.basename(self.parameters['Wrapper']), self.parameters['JobName'],bossJobType, self.parameters['JobName'],self.parameters['Wrapper'],self.parameters['Tarball'], self.parameters['JobName'], self.parameters['JobName']))
+    declareClad.write(" <program> <exec><![CDATA[%s]]></exec><args><![CDATA[""]]></args><stderr><![CDATA[%s.stderr]]></stderr><program_types><![CDATA[%s]]></program_types><stdin><![CDATA[""]]></stdin><stdout><![CDATA[%s.stdout]]></stdout><infiles><![CDATA[%s,%s]]></infiles><outfiles><![CDATA[*.root,%s.stdout,%s.stderr,FrameworkJobReport.xml]]></outfiles><outtopdir><![CDATA[""]]></outtopdir></program></chain></task>"% (os.path.basename(parameters['Wrapper']), parameters['JobName'],bossJobType, parameters['JobName'],parameters['Wrapper'],parameters['Tarball'], parameters['JobName'], parameters['JobName']))
     declareClad.close()
 #    logging.debug("SubmitterInterface:BOSS xml declare file written:%s" % xmlfile)
 
@@ -245,62 +151,6 @@ def declare(bossCfgDir,parameters):
     try:
         #bossJobId = bossJobId.split("TASK_ID:")[1].split("\n")[0].strip()
         bossJobId = bossJobId.split(":")[1].split("\n")[0].strip()
-    except StandardError, ex:
-        #logging.debug("SubmitterInterface:BOSS Job ID: %s. BossJobId set to 0\n" % bossJobId)
-        raise ProdAgentException("Job Declaration Failed")
-    #os.remove(xmlfile)
-    return bossJobId
-
-
-
-def declareBulk(bossCfgDir, jobList, inpSandbox, workingDir , workflow ):
-    """
-    BOSS4declareBulk
-
-    BOSS 4 command to declare a task from a list of jobSpec paths
-    """
-
-    # xml file name
-    xmlfile ="%s/%s-declare.xml"% ( workingDir , workflow )
-    declareClad=open( xmlfile,"w" )
-    declareClad.write(
-        "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n"
-        )
-    declareClad.write( "<task name=\"%s\">\n" % workflow )
-
-    # jobType retrieval
-    bossQuery = "boss showProgramTypes -c " + bossCfgDir
-    queryOut = executeCommand(bossQuery)
-    bossJobType = "cmssw"
-    if queryOut.find("cmssw") < 0:
-        bossJobType=""
-
-    # wrapper filename
-    wrapperName = "%s/%s-submit" % (workingDir, workflow)
-    parameters = { 'Wrapper' : wrapperName }
-
-    jobSpecList = jobList#.split('\n')
-    jobSpecUsedList = []
-
-    for jobSpecId, cacheDir in jobList.items():
-        if len(jobSpecId) == 0 or  jobSpecId in jobSpecUsedList : continue
-        jobSpecUsedList.append( jobSpecId )
-        parameters['JobName'] = jobSpecId
-        parameters['Tarball'] = inpSandbox
-        declareClad.write( chainTemplate(parameters, bossJobType) )
-
-    declareClad.write("</task>\n")
-    declareClad.close()
-
-    # actual BOSS declaration
-    bossDeclare = "boss declare -xmlfile %s"%xmlfile + "  -c " + bossCfgDir
-    bossJobId = executeCommand(bossDeclare)
-#    print bossJobId
-    #logging.debug( bossJobId)
-    try:
-        #bossJobId = bossJobId.split("TASK_ID:")[1].split("\n")[0].strip()
-        bossJobId = bossJobId.split(":")[1].split("\n")[0].strip()
-#        print "bossJobId", bossJobId
     except StandardError, ex:
         #logging.debug("SubmitterInterface:BOSS Job ID: %s. BossJobId set to 0\n" % bossJobId)
         raise ProdAgentException("Job Declaration Failed")
@@ -467,8 +317,6 @@ def executeCommand(command,timeout=600):
     # f.write("output=%s"%output)
     # f.close()
     return output
-
-
 # Stijn suggestion for child processes. Thanks.
 def stoppid(pid,sig):
     """
@@ -536,7 +384,6 @@ def getoutput(jobId,directory,bossCfgDir):
     outp=outfile
     return outp
 
-
 def reportfilename(jobId,directory):
     """
     BOSS4reportfilename
@@ -569,17 +416,13 @@ def jobSpecId(id,bossCfgDir):
     try:
         taskid=id.split('.')[0]
     except:
-        return ""
-    try:
-        chainid=id.split('.')[1]
-    except:
         #logging.error("Boss4 JobSpecId splitting error")
-        chainid="1"
-    outfile=executeCommand("bossAdmin SQL -query \"select NAME from CHAIN where TASK_ID=" + taskid + " AND ID=" +  chainid  + "\" -c " + bossCfgDir)
+        return ""
+    outfile=executeCommand("bossAdmin SQL -query \"select TASK_NAME from TASK where id='%s'\""%taskid + " -c " + bossCfgDir)
 
     outp=outfile
     try:
-        outp=outp.split("NAME")[1].strip()
+        outp=outp.split("TASK_NAME")[1].strip()
     except:
         outp=""
     subDir=subdir(id,bossCfgDir)
@@ -667,8 +510,6 @@ def scheduler(id,bossCfgDir,ended=""):
     #logging.debug("BOSS4scheduler outp '%s'"%outp)    
 
     return outp
-
-
 def taskEnded(id,bossCfgDir):
     """
     taskEnded
@@ -740,13 +581,13 @@ def isBOSSDeclared(Wrapper,JobName):
     content = file(idFile).read().strip()
     content=content.replace("JobId=", "")
     try:
-        jobId = content
+        jobId = int(content)
     except ValueError:
         jobId = None
     return jobId
 
-
 def FailedSubmission(bossJobId,bossCfgDir):
+
     
     taskid=bossJobId.split('.')[0]
     try:
@@ -770,27 +611,11 @@ def FailedSubmission(bossJobId,bossCfgDir):
             
         except:
             pass
+        
+def Archive(jobId,bossCfgDir):
 
-
-def archive(jobId,bossCfgDir):
-    """
-    BOSS4archive
-
-    Boss 4 command to manually archive jobs in the BOSS db
-    (i.e. move jobe entries to ENDED_ tables )
-    """
-    #logging.debug("Boss4 getoutput start %s "%jobId)
-    #print "Boss4 getoutput start %s "%jobId
-    try:
-        taskid = jobId[0].split('.')[0]
-        chainid=jobId[0].split('.')[1]
-        resub=jobId[0].split('.')[2]
-    except:
-        pass
-
-    outfile=executeCommand("boss archive -taskid %s -jobid %s"%(taskid,chainid) + " -c " + bossCfgDir)
-    outp=outfile
-    return outp
+    outfile=executeCommand("boss archive -taskid %s -c %s"%(jobId.split('.')[0],bossCfgDir))
+    return
 
 
 def Delete(jobId,bossCfgDir):
