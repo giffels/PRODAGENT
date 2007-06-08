c4638ba1ab4a1e8f7b2458415d7ae6b3a46b484e
evansde <>
2007-06-08 14:51:40 +0000

    Add API module for ResourceControl containing some useful methods that plugins and other components can use to access site information from the RCDB

---
+++ src/python/ProdAgent/ResourceControl/ResourceControlAPI.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+"""
+_ResourceControlAPI_
+
+Basic API calls for components and plugins to access information
+from the ResourceControlDB
+
+"""
+
+import logging
+from  ProdAgent.ResourceControl.ResourceControlDB import ResourceControlDB
+
+from ProdCommon.MCPayloads.JobSpec import JobSpec
+from ProdAgentDB.Config import defaultConfig as dbConfig
+from ProdCommon.Database import Session
+
+
+def activeSiteData():
+    """
+    _activeSiteData_
+
+    Return a list of dictionaries for all active sites containing
+    SE, CE, Site Name, Site Index
+
+    """
+    
+    Session.connect()
+    Session.start_transaction()
+    resourceControlDB = ResourceControlDB()
+    _SiteList = resourceControlDB.siteNames()
+    siteData = [ resourceControlDB.getSiteData(x) for x in _SiteList ]
+    siteData = [ x for x in siteData if x['Active'] == True ]
+    Session.commit_all()
+    Session.close_all()
+    return siteData
+
+
+def createSiteNameMap():
+    """
+    _createSiteNameMap_
+
+    For the set of Active sites, generate a dictionary mapping
+    SiteIndex: SiteName
+    SEName: SiteName
+    CEName: SiteName
+
+    """
+    siteData = activeSiteData()
+    siteMap = {}
+    for site in siteData:
+        siteMap[site['SiteIndex']] = site['SiteName']
+        siteMap[site['CEName']] = site['SiteName']
+        siteMap[site['SEName']] = site['SiteName']
+    return siteMap
+
+def createSiteIndexMap():
+    """
+    _createSiteIndexMap_
+
+    For the set of Active sites, generate a dictionary mapping
+    SiteName: SiteIndex
+    SEName: SiteIndex
+    CEName: SiteIndex
+
+    """
+    siteData = activeSiteData()
+    siteMap = {}
+    for site in siteData:
+        siteMap[site['SiteName']] = site['SiteIndex']
+        siteMap[site['CEName']] = site['SiteIndex']
+        siteMap[site['SEName']] = site['SiteIndex']
+    return siteMap
+
+
+
+def createCEMap():
+    """
+    _createCEMap_
+
+    For the set of Active sites, generate a dictionary mapping 
+    SiteName : CEName 
+    SEName: CEName
+    SiteIndex: CEName
+
+    Useful for submitter plugings to translate sites to CEs
+    
+    """
+    siteData = activeSiteData()
+
+    ceMap = {}
+    for site in siteData:
+        ceMap[site['SiteIndex']] = site['CEName']
+        ceMap[site['SiteName']] = site['CEName']
+        ceMap[site['SEName']] = site['CEName']
+
+    return ceMap
+
+
+def createSEMap():
+    """
+    _createSEMap_
+
+    For the set of Active sites, generate a dictionary mapping
+    SiteName : SEName
+    CEName : SEName
+    SiteIndex : SEName
+
+    """
+    siteData = activeSiteData()
+    seMap = {}
+    for site in siteData:
+        seMap[site['SiteIndex']] = site['SEName']
+        seMap[site['SiteName']] = site['SEName']
+        seMap[site['CEName']] = site['SEName']
+
+    return seMap
+
+
+    
