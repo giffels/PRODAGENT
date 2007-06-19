5c9261011d7349b2f1ea8503b6c63035590f50c2
ckavka <>
2007-06-19 16:31:33 +0000

    * Replaced BOSS API call to get job status. * Added code to handle proxy settings. * Updated kill task method.

--- src/python/JobKiller/Killers/BOSSKiller.py
+++ src/python/JobKiller/Killers/BOSSKiller.py
@@ -6,11 +6,12 @@ Killer plugin for killing BOSS jobs
 
 """
 
-__revision__ = "$Id$"
-__version__ = "$Revision$"
+__revision__ = "$Id: BOSSKiller.py,v 1.3 2007/05/04 14:17:22 ckavka Exp $"
+__version__ = "$Revision: 1.3 $"
 __author__ = "Carlos.Kavka@ts.infn.it"
 
 import logging
+import os
 
 from JobKiller.Registry import registerKiller
 from JobKiller.KillerExceptions import InvalidJobException, \
@@ -18,7 +19,10 @@ from JobKiller.KillerExceptions import InvalidJobException, \
  
 from BossSession import BossSession, SchedulerError, BossError
 
-from JobState.JobStateAPI import JobStateInfoAPI, JobStateChangeAPI
+from BossSession import SUBMITTED
+
+from JobState.JobStateAPI import  JobStateChangeAPI, \
+                                  JobStateInfoAPI
 
 from ProdAgentCore.ProdAgentException import ProdAgentException
 
@@ -29,6 +33,7 @@ class BOSSKiller:
     """
 
     bossConfigDir = None
+    componentDir = None
 
     def __init__(self, args):
         """
@@ -37,8 +42,9 @@ class BOSSKiller:
 
         try:
             self.bossConfigDir = args['bossConfigDir']
+            self.componentDir = args['ComponentDir']
         except KeyError:
-            msg = "BOSS config directory is not defined"
+            msg = "BOSS config and/or component directory is not defined"
             logging.error(msg)
             raise Exception, msg
 
@@ -74,7 +80,7 @@ class BOSSKiller:
             raise Exception, msg
 
         # set number of executions to be equal to the maximum number of
-        # allowed racers so jobs will not be resubmitted, or even
+        # allowed retries so jobs will not be resubmitted, or even
         # not submitted at all if they have not been submitted yet
         try:
             JobStateChangeAPI.doNotAllowMoreSubmissions([jobSpecId])
@@ -87,7 +93,7 @@ class BOSSKiller:
         # get job information from BOSS
         try:
             bossSession = BossSession(self.bossConfigDir)
-            task = bossSession.loadByName(jobSpecId)
+            task = bossSession.loadByJobName(jobSpecId)
 
             # should be single task
             if not len(task) == 1:
@@ -287,26 +293,41 @@ class BOSSKiller:
 
         logging.info("BOSSKiller.killTask(%s)" % taskSpecId)
 
-        # verify that the task exists and it is in killable state
-        # TO BE ADDED
-
-        # update task database if necessary, may be not since the 
-        # TaskTracking component will take care of it
-        # TO BE ADDED
-
         # get task information from BOSS
         try:
-            bossSession = BossSession(self.bossConfigDir)
-            task = bossSession.loadByName(taskSpecId)
-            
-            # should be single task
+
+            # get the user's proxy if specified
+            splittedPayload = taskSpecId.split(':') 
+
+            # export it if necessary
+            if len(splittedPayload) > 1:
+                logging.info("requires proxy export")
+
+                # perform proxy export 
+                proxy = splittedPayload[1]
+                taskSpecId = splittedPayload[0]
+                previousEnv = os.environ['X509_USER_PROXY']
+                os.environ['X509_USER_PROXY'] = str(proxy)
+                logging.info("proxy environment set = " + \
+                             str(os.environ['X509_USER_PROXY']))
+
+            # create a BOSS session
+            bossSession = BossSession(self.bossConfigDir, '2', \
+                                      self.componentDir + '/bossLog.log')
+
+            # get task specification
+            task = bossSession.loadByJobName(taskSpecId)
+
+            # it should be single task
             if not len(task) == 1:
+
+                # no, signal error
                 msg = "Cannot get BOSS task information for %s\n" % taskSpecId
                 logging.error(msg)
                 raise JobNotSubmittedException, msg
 
         # deal with BOSS specific error
-        except (SchedulerError, BossError), err:
+        except BossError, err:
             msg = "Cannot get information for task %s, BOSS error: %s" % \
                   (taskSpecId, str(err))
             logging.error(msg)
@@ -315,11 +336,89 @@ class BOSSKiller:
         # get task
         taskId = task.keys()[0]
 
-        # kill all jobs in task
-        task[taskId].kill("all")
+        # select jobs names
+        logging.info("Taskid: "+ str(taskId))
+        jobSpecId = []
+        try:
+            njobs = task[taskId].load(SUBMITTED)
+            jobs = task[taskId].jobsDict()
+            logging.info("LOAD RESULT: "+ str(njobs) + "     " + str(jobs))
 
-        # remove information from BOSS database
-        task[taskId].archive("all")
+            # process all jobs
+            for jobid, jobMap in jobs.iteritems():
+                job = task[taskId].Job(jobid)
+                jobSpecId.append(job.staticInfo()["NAME"])
+
+        # error
+        except (BossError), err:
+            msg = "Cannot get information for task %s, BOSS error: %s" % \
+                  (taskSpecId, str(err))
+            logging.error(msg) 
+        logging.info("Now controll jobs status: ")
+       
+        # is there any job? 
+        if len(jobSpecId) > 0:
+
+            # yes, process all of them
+            for jid in jobSpecId:   
+
+                # verify that the job exists
+                try:
+                    stateInfo = JobStateInfoAPI.general(str(jid))
+
+                    # verify that it has not finished
+                    if stateInfo['State'] in ["finished"]:
+                        msg = "job %s is terminated, cannot be killed\n" % \
+                              str(jid)
+                        logging.info(msg)
+                        del jid
+
+                # error, remove it
+                except StandardError, ex:
+                    msg = "Cannot retrieve JobState Information for %s\n" % \
+                          str(jid)
+                    msg += str(ex)
+                    logging.info(msg)
+                    del jid
+
+            # do not allow resubmisions for it
+            try:
+                logging.info("JobSpecId list: "+ str(jobSpecId)) 
+                JobStateChangeAPI.doNotAllowMoreSubmissions(jobSpecId)
+
+            # error, operation failed
+            except ProdAgentException, ex:
+                msg = "Cannot stop resubmissions for jobs %s: " % \
+                      str(jobSpecId)
+                msg += str(ex)
+                logging.error(msg)
+                raise
+
+            # kill all jobs in task
+            logging.info("Try to kill Task "+str(taskId))
+        
+            try: 
+                task[taskId].kill("all")
+
+            # error
+            except SchedulerError, err:
+                msg = "Warning : Some jobs may not been killed, probably " + \
+                      "because they are in done status: "+ str(err) + " " + \
+                      str(SchedulerError)
+                logging.error(msg)
+             
+            # remove information from BOSS database
+            task[taskId].archive("all")
+            logging.info("Task killed and Archived")  
+       
+        # no jobs to kill 
+        else:
+            logging.info("No jobs to kill in BOSS DB")
+        
+        # restore old proxy if different from current one
+        if len(splittedPayload) > 1:
+            if previousEnv != str(proxy):
+                os.environ['X509_USER_PROXY'] = previousEnv
 
         return
 
