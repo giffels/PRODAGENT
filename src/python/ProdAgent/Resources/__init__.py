dfb107b3c5d45e8822de29d988a991cd0b8437aa
afanfani <>
2007-07-04 22:20:29 +0000

    factor out common LSF support code (hufnagel)

---
+++ src/python/ProdAgent/Resources/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+_Resources_
+
+Package for common methods in different execution environments
+
+"""
+__all__ = []
+__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: __init__.py,v 1.1 2007/06/19 20:57:34 hufnagel Exp $"
+
