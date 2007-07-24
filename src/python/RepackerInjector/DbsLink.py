af2e107a4ed80fffab9c3147f50469acf53920ce
hufnagel <>
2007-07-24 14:27:42 +0000

    some cleanup

--- src/python/RepackerInjector/DbsLink.py
+++ src/python/RepackerInjector/DbsLink.py
@@ -1,6 +1,6 @@
 
 import logging
-import ProdAgentCore.LoggingUtils as LoggingUtils
+
 from DBSAPI.dbsApi import DbsApi
 from DBSAPI.dbsException import *
 from DBSAPI.dbsApiException import *
@@ -8,9 +8,10 @@ from ProdCommon.DataMgmt.DBS.DBSErrors import DBSReaderError, formatEx
 
 
 class DbsLink:
+
     def __init__(self,**kw):
         self.con=None
-        print "Connecting to DBS [%s]"%(kw['url'],)
+        logging.info("Connecting to DBS [%s]" % kw['url'])
         try:
             self.con = DbsApi(kw)
         except DbsException, ex:
@@ -32,31 +33,28 @@ class DbsLink:
         try:
             self.con.updateFileMetaData(lfn,status_word)
         except Exception,ex:
-            print "Can not set status, error",ex
+            logging.info("Can not set status: %s" % ex)
             return -1
         return 0
 
 
 
     def poll_for_files(self,pri_ds,pro_ds,run):
-        dbspath="/"+pri_ds+"/"+pro_ds+"/RAW"
+        datasetpath="/"+pri_ds+"/"+pro_ds+"/RAW"
         res=[]
         try:
-            file_list=self.con.listLFNs(path=dbspath, queryableMetaData="NOTSET")
-            #print "FILELIST:",file_list
-            #file_list=self.con.listLFNs(path=dbspath)
+            file_list=self.con.listLFNs(path=datasetpath, queryableMetaData="NOTSET")
+            #file_list=self.con.listLFNs(path=datasetpath)
             for i in file_list:
                 lfn=i['LogicalFileName']
-
                 #check if LFN belongs to correct run
                 lumiList = self.con.listFileLumis(lfn)
-                #print "LUMILIST:",lumiList
                 file_lumis=[]
                 for lumi in lumiList:
                     if lumi['RunNumber'] == run:
                         file_lumis.append(lumi)
 
-                if(len(file_lumis)):
+                if( len(file_lumis) > 0 ):
                     tags_list=i['FileTriggerMap']
                     tags={}
                     for t in tags_list:
@@ -65,11 +63,11 @@ class DbsLink:
                         if(v>0 or len(n)>0):
                             tags[n]=v
                         else:
-                            print "Ignoring empty tag [%s:%s]"%(n,v)
-                            print lfn,tags
+                            #print "Ignoring empty tag [%s:%s]"%(n,v)
+                            #print lfn,tags
                             res.append((lfn,tags,file_lumis))
         except Exception,ex:
-            print "ERROR:",ex
+            logging.info("ERROR: %s" % ex)
             return []
         print res
         return res
