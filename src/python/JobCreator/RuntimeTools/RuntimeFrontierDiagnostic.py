cc4c75b702c1757d6c2302a9c1bd03e5fa2e7080
evansde <>
2007-08-20 16:45:54 +0000

    Add simple diagnostic error and warning count to frontier diagnostic

--- src/python/JobCreator/RuntimeTools/RuntimeFrontierDiagnostic.py
+++ src/python/JobCreator/RuntimeTools/RuntimeFrontierDiagnostic.py
@@ -21,7 +21,29 @@ def frontierDiagnostic():
     frontierLog = os.path.join(os.getcwd(), "Frontier.log")
     print "Running diagnostic on Frontier log:"
     print frontierLog
+    if not os.path.exists("Frontier.log"):
+        msg = "Frontier.log not found..."
+        print msg
+        return
 
+    errors = 0
+    warnings = 0
+    print "=============Dump Frontier Log=============="
+    handle = open("Frontier.log", 'r')
+    for line in handle.readlines():
+        print line
+        if line.startswith("error"):
+            errors += 1
+        if line.startswith("warn"):
+            warnings += 1
+    print "=========End Dump Frontier Log=============="
+
+    msg = "==========Frontier Summary==========\n"
+    msg += "  Warnings : %s\n" % warnings
+    msg += "  Errors   : %s\n" % errors
+    msg += "======End Frontier Summary==========\n"
+    print msg
+    
     return
 
 
