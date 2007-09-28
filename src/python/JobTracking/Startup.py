b835c8de55d555fedbe5691d538e2a6f62b3f354
ckavka <>
2007-09-28 15:01:10 +0000

    * Added new parameters processing.

--- src/python/JobTracking/Startup.py
+++ src/python/JobTracking/Startup.py
@@ -6,7 +6,7 @@ Start the component, reading its configuration from
 the common configuration file, which is accessed by environment variable
 
 """
-__revision__ = "$Id: Startup.py,v 1.3 2006/05/01 11:46:22 elmer Exp $"
+__revision__ = "$Id: Startup.py,v 1.4 2006/08/07 15:02:57 evansde Exp $"
 
 import os
 import sys
@@ -24,12 +24,33 @@ from JobTracking.TrackingComponent import TrackingComponent
 try:
     config = loadProdAgentConfiguration()
     compCfg = config.getConfig("JobTracking")
+
+    # BOSS configuration
+    bossConfig = config.get("BOSS")
+    if 'configDir' in bossConfig.keys():
+        compCfg['configDir'] = bossConfig['configDir']
+
+    # ProdAgent configuration
+    paConfig = config.get("ProdAgent")
+    if 'ProdAgentWorkDir' in paConfig.keys():
+        compCfg['ProdAgentWorkDir'] = paConfig['ProdAgentWorkDir']
+
+    try:
+        # ProdAgent configuration
+        jobCreatorConfig = config.get("JobCreator")
+        if 'ComponentDir' in jobCreatorConfig.keys():
+            compCfg['JobCreatorComponentDir'] = \
+                                              jobCreatorConfig['ComponentDir']
+    except AttributeError:
+        compCfg['JobCreatorComponentDir'] = ""
+        
+
 except StandardError, ex:
     msg = "Error reading configuration:\n"
     msg += str(ex)
+    import traceback
+    msg += traceback.format_exc()
     raise RuntimeError, msg
-
-
 
 compCfg['ComponentDir'] = os.path.expandvars(compCfg['ComponentDir'])
 
