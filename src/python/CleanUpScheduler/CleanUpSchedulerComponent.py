e851157009cec45436b993c37e8cba1d1de1b731
ahmadh <>
2007-12-09 17:10:12 +0000

    CleanUpScheduler component that polls DBS periodically and then initiates cleanUp Job to delete those unmerged LFN's which have already been Merged

--- src/python/CleanUpScheduler/CleanUpSchedulerComponent.py
+++ src/python/CleanUpScheduler/CleanUpSchedulerComponent.py
@@ -19,7 +19,7 @@ from MergeSensor.MergeCrossCheck import MergeSensorCrossCheck
 from MergeSensor.MergeCrossCheck import listAllMergeDatasets 
 import ProdCommon.MCPayloads.CleanUpTools as CleanUpTools
 from ProdCommon.DataMgmt.DBS.DBSReader import DBSReader
-
+import time
 
 
 
@@ -47,7 +47,7 @@ class CleanUpTasks(dict):
         """
         if not self.has_key(blockOrSite):
             self[blockOrSite] = []
-        self[blockOsSite].extend(lfns)
+        self[blockOrSite].extend(lfns)
         return
     
     
@@ -63,7 +63,7 @@ class CleanUpSchedulerComponent:
     def __init__(self, **args):
         self.args = {}
         self.args['Logfile'] = None
-        
+                
         self.args.update(args)
         if self.args['Logfile'] == None:
             self.args['Logfile'] = os.path.join(self.args['ComponentDir'],
@@ -71,8 +71,30 @@ class CleanUpSchedulerComponent:
         LoggingUtils.installLogHandler(self)
         msg = "CleanUpScheduler Started:\n"
         logging.info(msg)
-
+        #cleanup specs directory
+        self.args.setdefault("CleanupJobSpecs",os.path.join(self.args['ComponentDir'],'cleanup-specs'))
         
+        self.queueMode = False
+        
+        if str(self.args['QueueJobMode']).lower() == "true":
+            self.queueMode = True  
+
+        cycleDelay = int(self.args['CleanUpInterval'])
+        
+        seconds = str(cycleDelay % 60)
+        minutes = str((cycleDelay / 60) % 60)
+        hours = str(cycleDelay / 3600)
+
+        self.cleanUpDelay = hours.zfill(2) + ':' + minutes.zfill(2) + ':' + seconds.zfill(2)
+        logging.debug("cleanUpInterval")
+        logging.debug(self.cleanUpDelay)
+        
+        self.lfnLimit = int(self.args['LFNLimit'])
+        
+        #dbs reader parameters
+        self.dbsReader = None
+        self.dbsDelay = 100
+         
 
     def __call__(self, event, payload):
         """
@@ -106,10 +128,39 @@ class CleanUpSchedulerComponent:
         Run a polling cycle and publish the next cycle triggering event
 
         """
+        
         self.cycleOnce()
-        self.ms.publish("CleanUp:Cycle")
+                
+        #Generate new cleanup cycle after specified cleanup interval
+
+        self.ms.publish("CleanUp:Cycle","Cleanup cycle called",self.cleanUpDelay)
         self.ms.commit()
         return
+
+    def connectToDBS (self):
+        """
+        _connectToDBS_
+      
+        Getting dbs connection
+        Arguments:
+              None
+        Return:
+              None 
+
+        """
+           
+        connected = False
+        while not connected:
+
+          try:
+               
+              self.dbsReader = DBSReader(self.args['DBSURL'])
+          except (DBSReaderError,DBSConnectionError), ex:
+              logging.error("Failed to connect to DBS (Exception: %s) Trying again in %s seconds" % (str(ex),self.delayDBS))
+              time.sleep(self.delayDBS)
+          else:
+              connected = True 
+
 
     def cycleOnce(self):
         """
@@ -118,65 +169,166 @@ class CleanUpSchedulerComponent:
         Do a single update
 
         """
-        localDBS = self.args['DBSURL']
-        dbsReader = DBSReader(localDBS)
-        blockTasks = CleanUpTask()
+        #Connect to DBS
+        self.connectToDBS()
         
-        for unmergedDataset in listAllMergeDatasets():
-            mergedDataset = unmergedDataset.replace("-unmerged", "")
+        blockTasks = CleanUpTasks()
             
+               
+        try:
+           #Retrive list of Datasets currently being watched by MergeSensor
+           listAllMergeDataset = listAllMergeDatasets()
+        except Exception, ex:
+           logging.error("DBSReader Error: LIST DATASET failed, Exception: %s " % ex) 
+           return  
+        for unmergedDataset in listAllMergeDataset:
+                           
+            mergedDataset = unmergedDataset.replace("-unmerged", "")
+                      
             mergeXCheck = MergeSensorCrossCheck(unmergedDataset)
             unmergedToMergeFiles = mergeXCheck.getFileMap()
+                     
 
             #  //
             # // List of all merged LFNs known to merge sensor
             #//
-            mergedLFNs = [ v for v in unmergedToMergedFiles.values()
+            mergedLFNs = [ v for v in unmergedToMergeFiles.values() 
                            if v != None ]
-    
+           
+              
+
             #  //
             # // List of all merged LFNS that are known to DBS
             #//
-            goodMerges = dbsReader.crossCheck(mergedDataset, *mergedLFNs)
-
+            goodMerges = None
+            try:
+             goodMerges = self.dbsReader.crossCheck(mergedDataset, *mergedLFNs)
+            except Exception, ex:
+             logging.error("DBSReader crosscheck for goodmerges failed Exception: %s" % ex)
+             return  
+                      
 
             doneUnmergedFiles = []
-
+            doneMergedFiles = []
             for unmerged, merged in unmergedToMergeFiles.items():
                 if merged in goodMerges:
                     doneUnmergedFiles.append(unmerged)
-
+                    doneMergedFiles.append(merged)
+                    
+            
             #  //
             # // Get a list of block names for each LFN
             #//
             blockMap = mergeXCheck.getBlocksMap()
+            
+            i=0 
             for lfn in doneUnmergedFiles:
-                blockTasks.addFile(blockMap[lfn], lfn)
-
+                i=i+1
+                blockTasks.addFiles(blockMap[lfn], lfn)
+            
         #  //
         # // For each block name, find the site from DBS
         #//  Assume: Since we are dealing with unmerged files
         #  //Should be only one site per block.
         # // May want to add logic to check that....
         #//
-        siteTasks = CleanUpTask()
+                  
+         
+   
+        siteTasks = CleanUpTasks()
         for block in blockTasks.keys():
-            sites = dbsReader.listFileBlockLocation(block)
+            sites = self.dbsReader.listFileBlockLocation(block)
+            logging.debug("sites *********************************************")
+            #logging.debug(sites)
+            #logging.debug(block) 
             if len(sites) == 0:
                 # unknown location
                 continue
-            site = sites[-1]
-            siteTasks.addFiles(site, *blockTasks[block])
-            
-        #  //
-        # // TODO:
-        #//  
+            logging.debug('length')
+            logging.debug(len(sites))
+            site = sites[-1]            
+                        
+            siteTasks.addFiles(site, *blockTasks[block])       
+        
+           
+          
         #  //For each site in siteTasks, generate a cleanup job spec with 
         # // the list of files to be cleaned, then publish the
         #//  cleanup workflow, job spec etc
         
+        #generate cleanup workflow spec
+        cleanUpWFs = CleanUpTools.createCleanupWorkflowSpec()    
+    
+        wfspec=os.path.join(self.args['CleanupJobSpecs'],cleanUpWFs.payload.workflow + '-workflow.xml' )
+        cleanUpWFs.save(wfspec)
+
+        #Publishing newworkflow event
+        self.ms.publish("NewWorkflow", wfspec)
+        self.ms.commit() 
+
+        #Generating cleanup jobspecs with list of lfns to be deleted
+
+        cleanUpJobSpec = [] 
+
+        #generate cleanup jobspec having inputfiles lfns
+        for x in siteTasks.keys():
+        
+            
+            if self.lfnLimit<=0:
+               logging.info("No Job Generated, LFNLimit set less than or equal to 0")         
+               return
+            else:
+               njobs = len(siteTasks[x])/self.lfnLimit
+                
+               if (len(siteTasks[x])%self.lfnLimit) > 0 :               
+                njobs = njobs + 1
+               
+               ref=0
+               
+               for i in range (0,njobs):
+                                  
+                    
+                 logging.debug(siteTasks[x][ref:ref+self.lfnLimit]) 
+                 cleanUpJobSpec.append(CleanUpTools.createCleanupJobSpec(cleanUpWFs,x,*siteTasks[x][ref:ref+self.lfnLimit]))
+                                        
+                 ref=ref+self.lfnLimit 
+                    
+         
+        for i in range (0,len(cleanUpJobSpec)):
+          
+          jobspec=os.path.join(self.args['CleanupJobSpecs'],cleanUpJobSpec[i].parameters["JobName"] + ".xml") 
+          cleanUpJobSpec[i].save(jobspec)
+          logging.debug('JobSpec Saved')
+
+          #publishing jobspec
+          self.publishCreateJob(jobspec)
+         
+               
+   
         return
 
+    def publishCreateJob(self,cleanupSpecURL):
+        """
+        _publishCreateJob_
+
+        Publish create job event with cleanupSpecURL provided as PAYLOAD
+
+        Arguments:
+         cleanupSpecURL -- cleanup specification file name
+  
+        Return:
+         None
+
+        """
+
+        if self.queueMode:
+           self.ms.publish("QueueJob",cleanupSpecURL)
+        else:
+           self.ms.publish("CreateJob",cleanupSpecURL)
+
+        self.ms.commit()
+
+        return
 
     def startComponent(self):
         """
@@ -186,7 +338,9 @@ class CleanUpSchedulerComponent:
 
         """
 
- 
+        # create directory if necessary
+        if not os.path.exists(self.args["CleanupJobSpecs"]):
+          os.makedirs(self.args["CleanupJobSpecs"])
         # create message service
         self.ms  = MessageService()
         # register
@@ -197,14 +351,28 @@ class CleanUpSchedulerComponent:
         self.ms.subscribeTo("CleanUp:CycleOnce")
         self.ms.subscribeTo("CleanUpScheduler:StartDebug")
         self.ms.subscribeTo("CleanUpScheduler:EndDebug")
+
+        ## Debug level
+        self.ms.publish("RequestInjector:StartDebug","none")
+        self.ms.publish("JobCreator:StartDebug","none")
+        self.ms.publish("JobSubmitter:StartDebug","none")
+        self.ms.commit()
+  
+
+        # generate first cleanup cycle
+        self.ms.remove("CleanUp:Cycle")
+        self.ms.publish("CleanUp:Cycle", "")
+        self.ms.commit()
+
  
         # wait for messages
         while True:
             Session.set_database(dbConfig)
             Session.connect()
             Session.start_transaction()
-            type, payload = self.ms.get()
-            self.ms.commit()
+            
+            type, payload = self.ms.get()              
+            self.ms.commit()            
             logging.debug("CleanUpScheduler: %s, %s" % (type, payload))
             self.__call__(type, payload)
             Session.commit_all()
