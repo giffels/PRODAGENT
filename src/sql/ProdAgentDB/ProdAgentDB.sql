89d26c6373f03bd27de168ce6ae68a9d51b6591e
evansde <>
2007-12-19 14:17:15 +0000

    Remove StatTracker tables from DB Schema, also add unique constraint on LFN within dataset for MergeSensor for DBS-less merge, which doesnt affect existing merge sensor

--- src/sql/ProdAgentDB/ProdAgentDB.sql
+++ src/sql/ProdAgentDB/ProdAgentDB.sql
@@ -186,98 +186,6 @@ CREATE TABLE `ms_subscription` (
   FOREIGN KEY(`typeid`) references `ms_type`(`typeid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*
- * =====================StatTracker DB Tables================== 
- */
-
-/*
- * Table to record Job Success information	
- *
- */
-CREATE TABLE st_job_success (
-   job_index INT NOT NULL AUTO_INCREMENT,
-   job_spec_id VARCHAR(255) NOT NULL,
-   workflow_spec_id VARCHAR(255) NOT NULL,
-   exit_code INT,
-   task_name VARCHAR(255),		
-   status VARCHAR(255),
-   site_name VARCHAR(255),
-   host_name VARCHAR(255),
-   se_name VARCHAR(255),
-   events_read INT DEFAULT 0,
-   events_written INT DEFAULT 0,
-   job_type VARCHAR(255),
-   time TIMESTAMP NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-   PRIMARY KEY (job_index)
-
-) TYPE=InnoDB;
-
-create index st_success_spec_index on st_job_success (job_spec_id(255));
-
-/*
- * Table to store list based attributes of a successful Job
- *
- */
-CREATE TABLE st_job_attr (
-   attr_index INT NOT NULL AUTO_INCREMENT,
-   job_index INT NOT NULL,
-   attr_name VARCHAR(255),
-   attr_class ENUM("run_numbers", "output_files", "output_datasets", "input_files", 'timing'),
-   
-   attr_value BLOB,
-
-   FOREIGN KEY(job_index)
-     REFERENCES st_job_success(job_index)
-       ON DELETE CASCADE,
- 
-   PRIMARY KEY (attr_index)
-) TYPE=InnoDB;
-
-
-/*
- * Table to store failed job information
- *
- */
-CREATE TABLE st_job_failure (
-   job_index INT NOT NULL AUTO_INCREMENT,
-   job_spec_id VARCHAR(255) NOT NULL,
-   workflow_spec_id VARCHAR(255) NOT NULL,
-   exit_code INT,
-   task_name VARCHAR(255),		
-   status VARCHAR(255),
-   site_name VARCHAR(255),
-   host_name VARCHAR(255),
-   se_name VARCHAR(255),
-   error_type VARCHAR(255),
-   error_code INT,
-   error_desc BLOB,
-   job_type VARCHAR(255),
-   time TIMESTAMP NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-   PRIMARY KEY (job_index)
-) TYPE=InnoDB;
-
-create index st_failure_spec_index on st_job_failure (job_spec_id(255));
-/*
- * Table to store list based attributes of a failed Job
- *
- */
-CREATE TABLE st_job_fail_attr (
-   attr_index INT NOT NULL AUTO_INCREMENT,
-   job_index INT NOT NULL,
-   attr_name VARCHAR(255),
-   attr_class ENUM("timing" ,"run_numbers", "input_files"),
-   attr_value BLOB,
-   FOREIGN KEY(job_index)
-     REFERENCES st_job_failure(job_index)
-       ON DELETE CASCADE,
- 
-   PRIMARY KEY (attr_index)
-) TYPE=InnoDB;
-
-
-/*
- * =====================End StatTracker DB Tables================== 
- */
 
 /*
 A merge_dataset table stores information on datasets watched by the
@@ -416,7 +324,7 @@ CREATE TABLE merge_inputfile
     instance int NOT NULL default '0',
 
     PRIMARY KEY(id),
-
+    UNIQUE(dataset, name),
     FOREIGN KEY(dataset) references merge_dataset(id) ON DELETE CASCADE,
     FOREIGN KEY(mergedfile) references merge_outputfile(id) ON DELETE CASCADE,
     FOREIGN KEY(block) references merge_fileblock(id) ON DELETE CASCADE
