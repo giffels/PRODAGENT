9170b1fd5976c1f46c5c56cb1ac587f7178fc777
ahmadh <>
2008-02-08 11:14:05 +0000

    Added the Generic Method for retrieving configuration from ProdAgent Config

--- src/python/ProdAgentDB/Config.py
+++ src/python/ProdAgentDB/Config.py
@@ -2,7 +2,8 @@
 """
 _Config_
 
-Get Configuration for ProdAgentDB from the ProdAgent config file.
+Get Configuration from the ProdAgent config file by providing the block name provided in config block
+in ProdAgent config file.
 This should be available through PRODAGENT_CONFIG env var
 
 If the configuration is not available, the default config settings
@@ -11,17 +12,35 @@ here will be used.
 """
 import os
 import logging
-
 from ProdAgentCore.Configuration import loadProdAgentConfiguration
+first_access = True
 
-def loadConf():
+
+def loadConfig(db = "ProdAgentDB"):
+    """
+    loadConfig method that loads the configuration of database base from ProdAgentConfig.xml and return it asdictionary 
+    object
+
+    Argument:
+            db : database configuration block to load
+
+    Return:
+            Dictionary containing the required configuration  
+
+    """
     try:
+        global first_access
+        
         cfg = loadProdAgentConfiguration()
-        dbConfig = cfg.getConfig("ProdAgentDB")
-        defaultConfig.update(dbConfig)
+        dbConfig = cfg.getConfig(db)
+        if (first_access == True):
+          defaultConfig.update(dbConfig)
+                  
+        first_access = False
+        return dbConfig 
     except StandardError, ex:
-        msg = "ProdAgentDB.Config:"
-        msg += "Unable to load ProdAgent Config for ProdAgentDB\n"
+        msg = "%s.Config:" % db
+        msg += "Unable to load ProdAgent Config for " + db
         msg += "%s\n" % ex
         logging.warning(msg)
 
@@ -39,7 +58,7 @@ defaultConfig={'dbName':'ProdAgentDB',
                'dbWaitingTime' : 10 
               }
 
+#// called at startup to fill the defaultConfig dictionary with ProdAgentDB config :: BACKWARD COMPATIBILITY
+loadConfig()
+       
 
-loadConf()
-
-        
