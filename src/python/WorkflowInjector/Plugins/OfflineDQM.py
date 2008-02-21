490ec179b8c2ead96160f64325f428a60a42aad9
evansde <>
2008-02-21 13:59:13 +0000

    Add prototype for injecting OfflineDQM jobs

---
+++ src/python/WorkflowInjector/Plugins/OfflineDQM.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+"""
+_OfflineDQM_
+
+Plugin for injecting a set of analysis jobs for Offline DQM tools.
+
+These jobs are set to process the entire dataset in a single job,
+so the size of the dataset is pulled in and the split size
+set to the number of files in the dataset
+
+
+"""
+
+
+import logging
+
+
+
+
+from WorkflowInjector.PluginInterface import PluginInterface
+from WorkflowInjector.Registry import registerPlugin
+from ProdCommon.JobFactory.DatasetJobFactory import DatasetJobFactory
+from IMProv.IMProvLoader import loadIMProvFile
+from IMProv.IMProvQuery import IMProvQuery
+
+
+class OfflineDQM(PluginInterface):
+    """
+    _OfflineDQM_
+
+    Plugin to generate a set of analysis jobs for a workflow
+    containing an input dataset
+
+    """
+    def handleInput(self, payload):
+        logging.info("OfflineDQM: Handling %s" % payload)
+
+        
+        self.workflow = None
+        self.dbsUrl = None
+        self.loadPayloads(payload)
+
+        
+        
+        factory = DatasetJobFactory(self.workflow,
+                                    self.workingDir,
+                                    self.dbsUrl)
+        
+        self.splitType = "file"
+        self.splitSize = 1000000 # quick and nasty
+        
+        jobs = factory()
+        for job in jobs:
+            self.queueJob(job['JobSpecId'], job['JobSpecFile'],
+                          job['JobType'],
+                          job['WorkflowSpecId'],
+                          job['WorkflowPriority'],
+                          *job['Sites'])
+            
+                
+        return
+    
+    def loadPayloads(self, payloadFile):
+        """
+        _loadPayloads_
+        
+        
+        """
+        
+        
+        
+        self.workflow = self.loadWorkflow(workflowFile)
+        
+        
+        value = self.workflow.parameters.get("DBSURL", None)
+        if value != None:
+            self.dbsUrl = value
+
+        if self.dbsUrl == None:
+            msg = "Error: No DBSURL available for dataset:\n"
+            msg += "Cant get local DBSURL and one not provided with workflow"
+            logging.error(msg)
+            raise RuntimeError, msg
+
+        
+        return
+        
+registerPlugin(OfflineDQM, OfflineDQM.__name__)
--- src/python/WorkflowInjector/Plugins/__init__.py
+++ src/python/WorkflowInjector/Plugins/__init__.py
@@ -14,6 +14,7 @@ import DatasetScrambler
 import FileListFeeder
 import SoupKitchen
 import BlockFeeder
+import OfflineDQM
 
 
 
