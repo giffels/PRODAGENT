1554746c085b541d1c62700da08b0ea9709ddc81
ahmadh <>
2008-06-19 14:43:32 +0000

    Alert Handlers Package

---
+++ src/python/AlertHandler/Handlers/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+_init_
+Import Alert Handlers and Register them in ProdCommon registries 
+"""
+
+from MinorAlertHandler import MinorAlertHandler 
+from WarningAlertHandler import WarningAlertHandler
+from ErrorAlertHandler import ErrorAlertHandler
+from CriticalAlertHandler import CriticalAlertHandler
+ 
+
