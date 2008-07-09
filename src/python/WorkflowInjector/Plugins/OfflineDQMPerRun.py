526aab8908e425cb08bd8b5652f04b21462b7288
evansde <>
2008-07-09 00:59:07 +0000

    add state preservation of runs seen so that the same workflow can be published repeatedly and only new runs will be picked up

--- src/python/WorkflowInjector/Plugins/OfflineDQMPerRun.py
+++ src/python/WorkflowInjector/Plugins/OfflineDQMPerRun.py
@@ -11,13 +11,44 @@ These jobs are set to process the entire dataset in one job per run
 
 
 import logging
-
+import pickle
+import os
 
 
 
 from WorkflowInjector.PluginInterface import PluginInterface
 from WorkflowInjector.Registry import registerPlugin
 from ProdCommon.JobFactory.RunJobFactory import RunJobFactory
+
+
+class StateFile:
+    """
+    _StateFile_
+
+    pickleable state file for a particular workflow
+
+    """
+    def __init__(self):
+        self.workflow = None
+        self.runs = []
+
+    def save(self, filename):
+        """pickle self"""
+        handle = open(filename, 'w')
+        pickle.dump(self, handle)
+        handle.close()
+        return
+
+    def load(self, filename):
+        """unpickle self"""
+        handle = open(filename, 'r')
+        tmp = pickle.load(handle)
+        self.workflow = tmp.workflow
+        self.runs = tmp.runs
+        handle.close()
+        return
+
+
 
 
 class OfflineDQMPerRun(PluginInterface):
@@ -35,8 +66,19 @@ class OfflineDQMPerRun(PluginInterface):
         self.dbsUrl = None
         self.loadPayloads(payload)
         self.siteName = None
-        self.publishWorkflow(payload, self.workflow.workflowName())
-        self.publishNewDataset(payload)
+        self.state = StateFile()
+        self.state.workflow = self.workflow.workflowName()
+        self.stateFile = os.path.join(
+            self.workingDir, "%s-State.pkl" % self.workflow.workflowName())
+
+        if os.path.exists(self.stateFile):
+            logging.info("State file exists for workflow: %s" % self.stateFile)
+            self.state.load(self.stateFile)
+
+        else:
+            logging.info("No State file found, starting from scratch")
+            self.publishWorkflow(payload, self.workflow.workflowName())
+            self.publishNewDataset(payload)
 
 
         factory = RunJobFactory(self.workflow,
@@ -45,14 +87,19 @@ class OfflineDQMPerRun(PluginInterface):
 
 
         jobs = factory()
+
         for job in jobs:
+            if job['Run'] in self.state.runs:
+                msg = "Run %s already known, skipping..." % job['Run']
+                logging.info(msg)
+                continue
             self.queueJob(job['JobSpecId'], job['JobSpecFile'],
                           job['JobType'],
                           job['WorkflowSpecId'],
                           job['WorkflowPriority'],
                           *job['Sites'])
-
-
+            self.state.runs.append(job['Run'])
+        self.state.save(self.stateFile)
         return
 
     def loadPayloads(self, payloadFile):
