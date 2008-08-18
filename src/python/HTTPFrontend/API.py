7e9eec6e6f83e9c5dd03fd1a211cfa5c98cd5e9f
mcinquil <>
2008-08-18 13:36:05 +0000

    Self consistent db queries

--- src/python/HTTPFrontend/API.py
+++ src/python/HTTPFrontend/API.py
@@ -1,6 +1,52 @@
-from TaskTracking.TaskStateAPI import *
 import time, os, datetime,commands
 import logging
+# DB PA
+from ProdAgentCore.ProdAgentException import ProdAgentException
+from ProdAgentDB.Connect import connect
+from ProdAgentDB.Config import defaultConfig as dbConfig
+
+
+def openConnPA():
+    """
+    _openConnPA_
+     opening connection with the PA DB
+    """
+    conn=connect(False)
+    dbCur=conn.cursor()
+    return conn, dbCur
+
+def closeConnPA( conn, dbCur):
+    """
+    _closeConnPA_
+    closing connection with the PA DB
+    """
+    dbCur.close()
+    conn.close()
+
+def queryMethod( strQuery ):
+    """
+    _queryMethod_
+    standard method for doing queries
+    """
+    ## opening connection with PA's DB
+    conn, dbCur = openConnPA()
+    try:
+        dbCur.execute("START TRANSACTION")
+        try: 
+            dbCur.execute(strQuery)
+        except Exception,ex:
+            raise ProdAgentException("Query problems")
+        rows = dbCur.fetchall()
+        dbCur.execute("COMMIT")
+        ## closing connection with PA's DB
+        closeConnPA( dbCur, conn )
+        return rows
+    except:
+        dbCur.execute("ROLLBACK")
+        ## closing connection with PA's DB
+        closeConnPA( dbCur, conn )
+        logging.error( "Error quering PA DB! Query: " + strQuery)
+        raise
 
 def composeDestinationCondition(destination = 'all'):
     if destination == 'all' or destination == ():
@@ -15,7 +61,7 @@ def composeDestinationCondition(destination = 'all'):
 def getQueues(destination = 'all'):
     dest_condition = composeDestinationCondition(destination);
     queryString = "select distinct(destination) from bl_runningjob where 1 "+dest_condition
-    queues = queryMethod(queryString, None)
+    queues = queryMethod(queryString)
     return queues
 
 # # #
@@ -25,41 +71,41 @@ def getQueues(destination = 'all'):
 
 def getJs__tasInstance():
     queryString = "SELECT status, notificationSent FROM js_taskInstance;"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return taskCheck
 
 
 def getNumTaskFinished():
 
     queryString = "SELECT * FROM js_taskInstance  WHERE notificationSent > 1 and status <> \'killed\' and status <> \'not submitted\';"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return len(taskCheck)
 
 
 def getNumTaskSubmitted():
 
     queryString = "SELECT * FROM js_taskInstance  WHERE status = \'submitted\';"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return len(taskCheck)
 
 
 def getNumTaskNotFinished():
 
    queryString = "SELECT * FROM js_taskInstance  WHERE notificationSent < 2;"
-   taskCheck = queryMethod(queryString, None)
+   taskCheck = queryMethod(queryString)
    return len(taskCheck)
 
 def getNumTaskKilled():
 
    queryString = "SELECT * FROM js_taskInstance  WHERE status = \'killed\';"
-   taskCheck = queryMethod(queryString, None)
+   taskCheck = queryMethod(queryString)
    return len(taskCheck)
 
 
 def getNumTaskNotSubmitted():
 
    queryString = "SELECT * FROM js_taskInstance  WHERE status = \'not submitted\';"
-   taskCheck = queryMethod(queryString, None)
+   taskCheck = queryMethod(queryString)
    return len(taskCheck)
 
 def getNumTask(statusName,ended = True):
@@ -67,7 +113,7 @@ def getNumTask(statusName,ended = True):
         queryString = "SELECT * FROM js_taskInstance  WHERE status =\'"+statusName+ "\' and notficationSent =2;"
     else:
         queryString = "SELECT * FROM js_taskInstance  WHERE status =\'"+statusName+ "\' and notificationSent <2;"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return len(taskCheck)
 
 # # #
@@ -76,7 +122,7 @@ def getNumTask(statusName,ended = True):
 def getNumJobResubmitting():
 
     queryString = "select count(*) from ms_message where ms_message.dest = (select procid  from ms_process where name = \'JobSubmitter\');"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     num_rows=taskCheck[0]
     return num_rows[0]
 
@@ -86,12 +132,12 @@ def getNumJobResubmitting():
 #
 def getInfoRunningJobError():
     queryString = "select  wrapper_return_code, application_return_code, from bl_runningjob ;"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return taskCheck
 
 def getInfoRunningJobStatus():
     queryString = "SELECT status from bl_runningjob;"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return taskCheck
 
 
@@ -99,7 +145,7 @@ def getInfoRunningJobStatus():
 def getNum_Wrap_Error():
 
     queryString = "select count(*) from bl_runningjob where wrapper_return_code is not null or wrapper_return_code <> 0;"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     num_rows=taskCheck[0]
     return num_rows[0]
 
@@ -107,7 +153,7 @@ def getNum_Wrap_Error():
 def getNum_Application_Error():
 
     queryString = "select count(*) from bl_runningjob where application_return_code is not null or application_return_code <> 0;"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     num_rows=taskCheck[0]
     return num_rows[0]
 
@@ -118,7 +164,7 @@ def getList_Application_Error(destination='all'):
     else:
         dest_condition=" and destination like '%"+destination+"%'"
     queryString = "select application_return_code, count(*) from bl_runningjob where application_return_code <> 0 or application_return_code is not null "+dest_condition+" group by  application_return_code"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return taskCheck
 
 
@@ -130,14 +176,14 @@ def getList_Wapper_Error(destination='all'):
        dest_condition=" and destination like '%"+destination+"%'"
 
     queryString = "select wrapper_return_code, count(*) from bl_runningjob where wrapper_return_code <> 0 or application_return_code is not null "+dest_condition+" group by wrapper_return_code"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return taskCheck
 
 
 def getNumJobCleared():
 
     queryString = "SELECT count(*) from bl_runningjob where status = 'E';"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     num_rows=taskCheck[0]
     return num_rows[0]
 
@@ -145,7 +191,7 @@ def getNumJobCleared():
 def getNumJobNotCleared():
 
     queryString = "SELECT count(*) from bl_runningjob where status = 'SD';"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString,)
     num_rows=taskCheck[0]
     return num_rows[0]
 
@@ -153,7 +199,7 @@ def getNumJobNotCleared():
 def getBossLiteRunningJobs(key,destination='all'):
     dest_condition = composeDestinationCondition(destination);
     queryString = "select count("+key+"),"+key+" from bl_runningjob where 1 "+dest_condition+" group by "+key+" "
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     logging.info("====getBLRJ======> %s"%queryString)
     return taskCheck
 
@@ -162,7 +208,7 @@ def getNumBossLiteRunningJobs(key,date,destination='all'):
     dest_condition = composeDestinationCondition(destination);
     strDate = time.strftime('%Y-%m-%d %H:%M:%S',(time.gmtime(date)))
     queryString = "select count(*),"+key+" from bl_runningjob where submission_time < '"+strDate+"' "+dest_condition+" group by "+key+" " 
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return taskCheck
 
 # # # VM
@@ -176,7 +222,7 @@ def getNumLastBossLiteRunningJobs(key,past=0,destination='all'):
     else:
         DateCondition = "where 1 "
     queryString = "select count(*),"+key+" from bl_runningjob "+DateCondition+" "+dest_condition+" group by "+key+" " 
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     logging.info("=============> %s"%queryString)
     return taskCheck
 
@@ -193,14 +239,14 @@ def getLastDeltaTimeBossLiteRunningJobs(from_time,to_time, past=0,destination = 
        DateCondition = "where 1 "
       
     limitsQueryString = "select max("+to_time+"-"+from_time+") from bl_runningjob "+DateCondition+" and "+to_time+"-"+from_time+">0 and "+to_time+"-"+from_time+"<31536000 "+dest_condition+";"
-    max = queryMethod(limitsQueryString, None)
+    max = queryMethod(limitsQueryString)
     if str(max[0][0]) != 'None':   #db gives NULL result
        max = float(max[0][0]);
     else:
        max = 3600.;
 
     limitsQueryString = "select count(*) from bl_runningjob "+DateCondition+" and "+to_time+"-"+from_time+">0 and "+to_time+"-"+from_time+"<31536000 "+dest_condition+";"
-    Nbin = queryMethod(limitsQueryString, None)
+    Nbin = queryMethod(limitsQueryString)
     if Nbin[0][0]>100:
        Nbin = Nbin[0][0]/10
     else:
@@ -213,7 +259,7 @@ def getLastDeltaTimeBossLiteRunningJobs(from_time,to_time, past=0,destination = 
     h = int(max / Nbin)+1
 
     queryString = "select count(*),floor(("+to_time+"-"+from_time+")/"+str(h)+") from bl_runningjob  "+DateCondition+" and "+to_time+"-"+from_time+">0 and "+to_time+"-"+from_time+"<31536000 "+dest_condition+" group by floor(("+to_time+"-"+from_time+")/"+str(h)+") "
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
    
     return max, Nbin, taskCheck
 
@@ -222,14 +268,14 @@ def getDeltaTimeBossLiteRunningJobs(from_time,to_time, destination = 'all'):
     dest_condition = composeDestinationCondition(destination);
       
     limitsQueryString = "select max("+to_time+"-"+from_time+") from bl_runningjob where "+to_time+"-"+from_time+">0 and "+to_time+"-"+from_time+"<31536000 "+dest_condition+";"
-    max = queryMethod(limitsQueryString, None)
+    max = queryMethod(limitsQueryString)
     if str(max[0][0]) != 'None':   #db gives NULL result
        max = float(max[0][0]);
     else:
        max = 3600.;
 
     limitsQueryString = "select count(*) from bl_runningjob where "+to_time+"-"+from_time+">0 and "+to_time+"-"+from_time+"<31536000 "+dest_condition+";"
-    Nbin = queryMethod(limitsQueryString, None)
+    Nbin = queryMethod(limitsQueryString)
     if Nbin[0][0]>100:
        Nbin = Nbin[0][0]/10
     else:
@@ -242,7 +288,7 @@ def getDeltaTimeBossLiteRunningJobs(from_time,to_time, destination = 'all'):
     h = int(max / Nbin)+1
 
     queryString = "select count(*),floor(("+to_time+"-"+from_time+")/"+str(h)+") from bl_runningjob where "+to_time+"-"+from_time+">0 and "+to_time+"-"+from_time+"<31536000 "+dest_condition+" group by floor(("+to_time+"-"+from_time+")/"+str(h)+") "
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
    
     return max, Nbin, taskCheck
 
@@ -261,7 +307,7 @@ def getSites(past=0,Sites='flat'):
     OverallCount = 0;
     if Sites=='flat':
        queryString = "select count(*),destination from bl_runningjob "+dateCondition+" group by destination;"
-       taskCheck = queryMethod(queryString, None)
+       taskCheck = queryMethod(queryString)
        for site in taskCheck:
 #         outputsite = (str(site[1])).split(':')[0]
           outputsite = str(site[1])
@@ -271,7 +317,7 @@ def getSites(past=0,Sites='flat'):
        for site in Sites.keys():
           dest_condition = composeDestinationCondition(Sites[site]);
           queryString = "select count(*),destination from bl_runningjob "+dateCondition+" "+dest_condition+"group by destination;"
-          taskCheck = queryMethod(queryString, None)
+          taskCheck = queryMethod(queryString)
           tmpcount = 0;
           for queue in taskCheck:
              tmpcount += queue[0]
@@ -291,7 +337,7 @@ def getNum_WrapperErrors(destination = 'all',past=0): #begin_t=0,end_t=time.time
     else:
        dateCondition = "submission_time < '"+strEnd+"' "
     queryString = "select count(*) from bl_runningjob where (wrapper_return_code is not null or wrapper_return_code <> 0) "+dest_condition+" and "+dateCondition+" ;"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     num_rows=taskCheck[0]
     return num_rows[0]
 
@@ -307,7 +353,7 @@ def getNum_ApplicationErrors(destination = 'all', past=0):
     else:
        dateCondition = "submission_time < '"+strEnd+"' "
     queryString = "select count(*) from bl_runningjob where (application_return_code is not null or application_return_code <> 0) "+dest_condition+" and "+dateCondition+" ;"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     num_rows=taskCheck[0]
     return num_rows[0]
 
@@ -323,7 +369,7 @@ def getList_ApplicationErrors(destination='all', past=0):
     else:
        dateCondition = "submission_time < '"+strEnd+"' "
     queryString = "select application_return_code, count(*) from bl_runningjob where (application_return_code <> 0 or application_return_code is not null) "+dest_condition+" and "+dateCondition+" group by  application_return_code"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return taskCheck
 
 # # # VM
@@ -338,7 +384,7 @@ def getList_WrapperErrors(destination='all',past=0):
     else:
        dateCondition = "submission_time < '"+strEnd+"' "
     queryString = "select wrapper_return_code, count(*) from bl_runningjob where (wrapper_return_code <> 0 or application_return_code is not null) "+dest_condition+" and "+dateCondition+" group by wrapper_return_code"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return taskCheck
 
 # # # VM
@@ -353,7 +399,7 @@ def getNumJobs(destination='all',past=0):
     else:
        dateCondition = "submission_time < '"+strEnd+"' "
     queryString = "select count(*) from bl_runningjob where "+dateCondition+" "+dest_condition+" ;"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     logging.info("=============> %s"%queryString)
     return taskCheck
 
@@ -369,7 +415,7 @@ def getNumSuccessJob(destination='all',past=0):
     else:
        dateCondition = "submission_time < '"+strEnd+"' "
     queryString = "select count(*) from bl_runningjob where getoutput_time>0 and "+dateCondition+" and application_return_code=0 and wrapper_return_code=0 "+dest_condition
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     logging.info("=============> %s"%queryString)
     return taskCheck
 
@@ -385,7 +431,7 @@ def getNumFailWrapperJob(destination='all',past=0):
     else:
        dateCondition = "submission_time < '"+strEnd+"' "
     queryString = "select count(*) from bl_runningjob where "+dateCondition+" and wrapper_return_code!=0 "+dest_condition
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     logging.info("=============> %s - %s"%(queryString, str(past)))
     return taskCheck
 
@@ -403,7 +449,7 @@ def getTimeNumBossLiteRunningJobs(key,begin_t=0,end_t=time.time()-time.altzone,d
     strEnd = time.strftime('%Y-%m-%d %H:%M:%S',(time.gmtime(end_t)))
     queryString = "select count(*),"+key+" from bl_runningjob where submission_time < '"+strEnd+"' and submission_time > '"+strBegin+"' "+dest_condition+" group by "+key+" "
     logging.info("=============> %s"%queryString)
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return taskCheck
 
 def getNumJobSubmitted(destination='all',begin_t=0,end_t=time.time()-time.altzone):
@@ -415,7 +461,7 @@ def getNumJobSubmitted(destination='all',begin_t=0,end_t=time.time()-time.altzon
     strEnd = time.strftime('%Y-%m-%d %H:%M:%S',(time.gmtime(end_t)))
     queryString = "select count(*) from bl_runningjob where submission_time > '"+strBegin+"' and submission_time < '"+strEnd+"' "+dest_condition
     logging.info("=============> %s"%queryString)
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return taskCheck
 
 def getNumJobRetrieved(destination='all',begin_t=0,end_t=time.time()-time.altzone,timetype="submission_time"):
@@ -427,7 +473,7 @@ def getNumJobRetrieved(destination='all',begin_t=0,end_t=time.time()-time.altzon
     strEnd = time.strftime('%Y-%m-%d %H:%M:%S',(time.gmtime(end_t)))
     queryString = "select count(*) from bl_runningjob where getoutput_time>0 and "+timetype+" > '"+strBegin+"' and "+timetype+" < '"+strEnd+"' "+dest_condition
     logging.info("=============> %s"%queryString)
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return taskCheck
 
 def getNumJobSuccess(destination='all',begin_t=0,end_t=time.time()-time.altzone,timetype="submission_time"):
@@ -439,7 +485,7 @@ def getNumJobSuccess(destination='all',begin_t=0,end_t=time.time()-time.altzone,
     strEnd = time.strftime('%Y-%m-%d %H:%M:%S',(time.gmtime(end_t)))
     queryString = "select count(*) from bl_runningjob where getoutput_time>0 and "+timetype+" > '"+strBegin+"' and "+timetype+" < '"+strEnd+"' and application_return_code=0 and wrapper_return_code=0 "+dest_condition
     logging.info("=============> %s"%queryString)
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return taskCheck
 
 
@@ -453,19 +499,19 @@ def getNumJobFailWrapper(destination='all',begin_t=0,end_t=time.time()-time.altz
     strEnd = time.strftime('%Y-%m-%d %H:%M:%S',(time.gmtime(end_t)))
     queryString = "select count(*) from bl_runningjob where getoutput_time > '"+strBegin+"' and getoutput_time < '"+strEnd+"' and application_return_code=0 and wrapper_return_code!=0 "+dest_condition
     logging.info("=============> %s"%queryString)
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return taskCheck
 
 def getList_Destination(begin_t=0): 
     strBegin = time.strftime('%Y-%m-%d %H:%M:%S',(time.gmtime(begin_t))) 
     queryString = "select distinct(destination) from bl_runningjob where destination is not null and destination!='' and submission_time > '"+strBegin+"'"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return taskCheck
 
 def getNumJobSubmittedBySite(begin_t=0): 
     strBegin = time.strftime('%Y-%m-%d %H:%M:%S',(time.gmtime(begin_t)))
     queryString = "select count(*),destination from bl_runningjob where submission_time>'"+strBegin+"' and destination is not null and destination!='' group by destination "
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return taskCheck
 
 
@@ -473,7 +519,7 @@ def getNumJobSubmittedBySite(begin_t=0):
 
 def getUser(from_time):
     queryString = "select count(distinct  bl_task.user_proxy ),  bl_task.user_proxy, bl_runningjob.creation_timestamp from bl_task join bl_runningjob where bl_task.id = bl_runningjob.task_id and bl_runningjob.creation_timestamp > DATE_SUB(Now(),INTERVAL "+str(from_time)+" DAY) group by bl_task.user_proxy order by creation_timestamp;"
-    taskCheck = queryMethod(queryString, None)
+    taskCheck = queryMethod(queryString)
     return taskCheck
 
 
@@ -519,4 +565,3 @@ def getpidof(procname,service):
         msg = [service,"PID : "+pid ]
 
     return msg
-
