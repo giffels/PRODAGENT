458d82e03ff7160334f5df17452c9ced3d323517
swakef <>
2008-08-26 16:28:45 +0000

    Add override test code

--- src/python/StageOut/StageOutMgr.py
+++ src/python/StageOut/StageOutMgr.py
@@ -369,7 +369,15 @@ if __name__ == '__main__':
     import time
     import StageOut.Impl
 
+    overrideParams = {
+        "command" : 'srmv2',
+        "option" : '-streams_num=1',
+        "se-name" : 'gfe02.hep.ph.ic.ac.uk',
+        "lfn-prefix" : 'srm://gfe02.hep.ph.ic.ac.uk:8443/srm/managerv2?SFN=/pnfs/hep.ph.ic.ac.uk/data/cms',
+        }
+
     mgr = StageOutMgr()
+    #mgr = StageOutMgr(**overrideParams)
     pfn = "/etc/hosts"
     lfn = "/store/test/stageOutTest-%s" % int(time.time())
 
