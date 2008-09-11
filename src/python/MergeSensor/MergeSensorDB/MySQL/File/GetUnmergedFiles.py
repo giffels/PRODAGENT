1578f20b7ee970c78a928a5c5e771826c53890ec
swakef <>
2008-09-11 16:19:11 +0000

    Order unmerged files with lumi section. Currently returns a file for each lumi section. Need to find a way to remove the duplicates on the database side.

--- src/python/MergeSensor/MergeSensorDB/MySQL/File/GetUnmergedFiles.py
+++ src/python/MergeSensor/MergeSensorDB/MySQL/File/GetUnmergedFiles.py
@@ -20,17 +20,20 @@ class GetUnmergedFiles(MySQLBase):
           __execute__
 	  
 	  """
-
-
             
           # get all unmerged files in a particular fileblock
+          # actually pulls one result per lumi section - need to remove dups
+          # Can we do this in the db?
           self.sqlCommand = """
-                     SELECT *
-                       FROM merge_inputfile
-                      WHERE dataset='""" + str(datasetId) + """'
+                     SELECT id,name,guid,eventcount,block,status,dataset,
+                         mergedfile,filesize,merge_inputfile.run,failures,
+                         instance
+                       FROM merge_inputfile, merge_lumi as lumi
+                      WHERE merge_inputfile.id = lumi.file_id AND
+                        dataset='""" + str(datasetId) + """'
                         AND block='""" + str(block['block'])  + """'
                         AND status='unmerged'
-                   ORDER BY filesize
+                   ORDER BY lumi.run, lumi.lumi
                      """
 	  try:
 	  		                    
@@ -43,8 +46,12 @@ class GetUnmergedFiles(MySQLBase):
 	  
 	     
           rows = self.format (result, dictionary= True)
-     
-	   
+          
+          ids = {}
+          output = []
+          for row in rows:
+              if not ids.has_key(row['id']):
+                  output.append(row)
+                  ids[row['id']] = None
+          rows = output
           return rows #//END
-
- 
