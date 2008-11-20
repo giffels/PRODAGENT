0c081008f446e96bbf908b67a9e576385d5adede
gowdy <>
2008-11-20 10:44:58 +0000

    add update of src/python/ProdCommon/DataMgmt/DBS and checkout of DLS

--- ops/setupscripts/control/prodagent_install.sh
+++ ops/setupscripts/control/prodagent_install.sh
@@ -27,15 +27,27 @@ rm -rf lib/RepackerInjector
 cd $MYTESTAREA
 cvs co -r PRODCOMMON_$PCVERSION PRODCOMMON
 cd PRODCOMMON
+echo "Update DBS area for new DBS version, remove once using new ProdCommon"
+cvs upd -r PRODCOMMON_0_12_7 src/python/ProdCommon/DataMgmt/DBS
 make
 
 cd $MYTESTAREA
 # pull the DBS client out of DBS
 cvs co -r DBS_$DBSVERSION DBS
 
+#pull the DLS client out of CVS
+cvs co -r DLS_$DLSVERSION DLS/Client
+cd DLS/Client
+make
+cd $MYTESTAREA
+
+
 # T0 specific components
 
 cvs co -r T0_0_0_2_pre23 T0
+
+# this is now included in the PA dependencies
 #cvs co -r WMCORE_REQMGR_0_0_1 WMCORE
+
 cvs co -r LUMIDB_0_0_2 LUMIDB/LumiWebService/Client/LumiWebClient
 
