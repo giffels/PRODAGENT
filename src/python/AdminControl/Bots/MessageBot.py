fd825ed0ce0f5aae9091ab04d8623e5f315e074e
spiga <>
2008-12-06 07:43:46 +0000

    changed time format according to cleanHistory method

--- src/python/AdminControl/Bots/MessageBot.py
+++ src/python/AdminControl/Bots/MessageBot.py
@@ -44,7 +44,7 @@ class MessageBot(BotInterface):
         #  //
         # // Clean Message History Older than 3 days
         #//
-        msgSvc.cleanHistory("72:00:00")
+        msgSvc.cleanHistory("72")
         msgSvc.commit()
 
         return
