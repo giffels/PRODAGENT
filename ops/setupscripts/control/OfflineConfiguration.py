6884a21da3c2d171d442ab936caf5f5aa2d9b9a6
dmason <>
2008-12-11 20:41:04 +0000

    bringing this up to date...

--- ops/setupscripts/control/OfflineConfiguration.py
+++ ops/setupscripts/control/OfflineConfiguration.py
@@ -2,57 +2,77 @@
 """
 _OfflineConfiguration_
 
-Configuration for mapping streams and datasets to the various controls and
-settings used to process each stream.
+Processing configuration for the Tier0.
 """
 
-__revision__ = "$Id: OfflineConfiguration.py,v 1.2 2008/09/22 15:18:58 sfoulkes Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: OfflineConfiguration.py,v 1.6 2008/10/24 19:20:49 sfoulkes Exp $"
+__version__ = "$Revision: 1.6 $"
 
-from T0.RecoConfig.ControlFile import Tier0Configuration
-from T0.RecoConfig.ControlFile import addDataset
+from T0.RunConfigCache.Tier0Config import addDataset
+from T0.RunConfigCache.Tier0Config import createTier0Config
+from T0.RunConfigCache.Tier0Config import setAcquisitionEra
+from T0.RunConfigCache.Tier0Config import setConfigVersion
+from T0.RunConfigCache.Tier0Config import setProcessingStyle
+from T0.RunConfigCache.Tier0Config import setRepackVersionMapping
 
-# Default settings for all configurations is that reco is
-# disabled.
-addDataset(Tier0Configuration, "Default", "Default", do_reco = False,
-           scenario = "collisions", OverrideCMSSW = "BOGUS")
+# Create the Tier0 configuration object
+tier0Config = createTier0Config()
 
-# Default framework version
-defaultCMSSWVersion = "CMSSW_2_1_12"
+# Set global parameters like the acquisition era and the version of
+# the configuration.
+setAcquisitionEra(tier0Config, "HAPPYHAPPYWARMFUZZY_T0TEST_WITHBUNNIESDANCINGAROUND")
+setConfigVersion(tier0Config, __version__)
 
-# Config files for each scenario.  Filenames must be URLs.  Currently
-# only the "file" protocol is supported.
+# Setup some useful defaults: processing version, reco framework version,
+# global tag.
+defaultProcVersion = "v3_UNTILWECATCHEMANDTURNTHEMINTOTASTYSTEW_ALA_MASON"
+defaultRecoVersion = "CMSSW_2_2_1"
+defaultGlobalTag = "CRAFT_ALL_V4::All"
+
+# Create a dictionary that associates a reco configuration with a scenario.
+# The configuration must be specified as a url.
 recoConfig = {}
-#recoConfig["cosmics"] = "file:///data/cmsprod/CMSSW/CMSSW_2_1_10/src/Configuration/GlobalRuns/python/recoT0DQM_EvContent_1_26_cfg.py"
-#recoConfig["cosmics"] = "file:///data/cmsprod/CMSSW/CMSSW_2_1_10/src/Configuration/GlobalRuns/python/recoT0DQM_EvContent_38T_1_3_cfg.py"
-#recoConfig["cosmics"] = "file:///data/cmsprod/CMSSW/CMSSW_2_1_11/src/Configuration/GlobalRuns/python/recoT0DQM_EvContent_38T_1_5_cfg.py"
-#recoConfig["cosmics"] = "file:///data/cmsprod/CMSSW/CMSSW_2_1_11/src/Configuration/GlobalRuns/python/recoT0DQM_EvContent_38T_1_5_cfg_2_1_11.py"
-#recoConfig["cosmics"] = "file:///data/cmsprod/CMSSW/CMSSW_2_1_11/src/Configuration/GlobalRuns/python/recoT0DQM_EvContent_38T_1_6_cfg.py"
-#recoConfig["cosmics"] = "file:///data/cmsprod/CMSSW/CMSSW_2_1_11/src/Configuration/GlobalRuns/python/recoT0DQM_EvContent_1_29_cfg.py"
-recoConfig["cosmics"] = "file:///data/cmsprod/CMSSW/CMSSW_2_1_12/src/Configuration/GlobalRuns/python/recoT0DQM_EvContent_40T_1_1_LazyDownload_cfg.py"
-#recoConfig["cosmics"] = "file:///data/cmsprod/CMSSW/CMSSW_2_1_12/src/Configuration/GlobalRuns/python/recoT0DQM_EvContent_38T_1_6_LazyDownload_cfg.py"
-#recoConfig["cosmics"] = "file:///data/cmsprod/CMSSW/CMSSW_2_1_12/src/Configuration/GlobalRuns/python/recoT0DQM_EvContent_1_29_LazyDownload_cfg.py"
+recoConfig["cosmics"] = "http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/Configuration/GlobalRuns/python/recoT0DQM_EvContent_38T_cfg.py?revision=1.20"
 
-def addStreamADataset(datasetName, scenarioName, cmsswVersion = None):
-    """
-    _addStreamADataset_
+# Create the default configuration.  Repacking is enabled and everything else
+# is turned off.  The default processing style is also set to "Bulk".
+setProcessingStyle(tier0Config, "Default", "Bulk")
+addDataset(tier0Config, "Default", "Default",
+           default_proc_ver = defaultProcVersion, hltdebug = False,
+           do_reco = False, do_alca = False, do_dqm = False)
 
-    Convience function for setting the configuration for datasets in the "A"
-    stream.  If a framework version is not passed in the default will be used.
-    """
-    if cmsswVersion == None:
-        cmsswVersion = defaultCMSSWVersion
-        
-    addDataset(Tier0Configuration, "A", datasetName, do_reco = True,
-               reco_configuration = recoConfig[scenarioName],
-               scenario = scenarioName,
-               reco_version = cmsswVersion
-               )
+# Configure the processing style for the various streams.
+setProcessingStyle(tier0Config, "A", "Bulk")
+setProcessingStyle(tier0Config, "HLTDEBUG", "Bulk")
 
-# Install configurations for the stream "A" datasets.
-addStreamADataset("MinimumBias", "cosmics")
-addStreamADataset("Cosmics", "cosmics")
-addStreamADataset("Calo", "cosmics")
+# Actual configuration for datasets.  The Calo, Cosmics and MinimumBias
+# datasets will be reconstructed.
+addDataset(tier0Config, "A", "Calo",
+           default_proc_ver = defaultProcVersion, scenario = "cosmics",
+           do_reco = True, global_tag = defaultGlobalTag,
+           reco_configuration = recoConfig["cosmics"],
+           reco_version = defaultRecoVersion)
+addDataset(tier0Config, "A", "Cosmics",
+           default_proc_ver = defaultProcVersion, scenario = "cosmics",
+           do_reco = True, global_tag = defaultGlobalTag,
+           reco_configuration = recoConfig["cosmics"],
+           reco_version = defaultRecoVersion)
+addDataset(tier0Config, "A", "MinimumBias",
+           default_proc_ver = defaultProcVersion, scenario = "cosmics",
+           do_reco = True, global_tag = defaultGlobalTag,
+           reco_configuration = recoConfig["cosmics"],
+           reco_version = defaultRecoVersion)
+
+# Setup the mappings between the framework version used to take a run and the
+# version that should be used to repack it.
+setRepackVersionMapping(tier0Config, "CMSSW_2_1_X_2008-08-16-0300", "CMSSW_2_1_9")
+setRepackVersionMapping(tier0Config, "CMSSW_2_1_0", "CMSSW_2_1_9")
+setRepackVersionMapping(tier0Config, "CMSSW_2_1_1", "CMSSW_2_1_9")
+setRepackVersionMapping(tier0Config, "CMSSW_2_1_2", "CMSSW_2_1_9")
+setRepackVersionMapping(tier0Config, "CMSSW_2_1_4", "CMSSW_2_1_9")
+setRepackVersionMapping(tier0Config, "CMSSW_2_1_4_ONLINE1", "CMSSW_2_1_9")
+setRepackVersionMapping(tier0Config, "CMSSW_2_1_5", "CMSSW_2_1_9")
+setRepackVersionMapping(tier0Config, "CMSSW_2_1_6", "CMSSW_2_1_9")
 
 if __name__ == '__main__':
-    print Tier0Configuration
+    print tier0Config
