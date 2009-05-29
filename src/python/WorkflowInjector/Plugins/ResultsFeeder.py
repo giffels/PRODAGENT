601a6b80e18eee2e22191e7dedb2eabc9ff1268a
ewv <>
2009-05-29 21:48:18 +0000

    Migrate to input dataset to GlobalDBS

--- src/python/WorkflowInjector/Plugins/ResultsFeeder.py
+++ src/python/WorkflowInjector/Plugins/ResultsFeeder.py
@@ -13,8 +13,8 @@ Merges a /store/user dataset into /store/results. Input parameters are
 
 """
 
-__revision__ = "$Id: ResultsFeeder.py,v 1.9 2009/05/14 07:11:54 giffels Exp $"
-__version__  = "$Revision: 1.9 $"
+__revision__ = "$Id: ResultsFeeder.py,v 1.10 2009/05/19 19:36:00 ewv Exp $"
+__version__  = "$Revision: 1.10 $"
 __author__   = "ewv@fnal.gov"
 
 import logging
@@ -60,6 +60,28 @@ def getInputDBSURL():
 
     return dbsConfig.get("DBSURL", None)
 
+
+def getGlobalDBSURL():
+    try:
+        config = loadProdAgentConfiguration()
+    except StandardError, ex:
+        msg = "Error reading configuration:\n"
+        msg += str(ex)
+        logging.error(msg)
+        raise RuntimeError, msg
+
+    try:
+        dbsConfig = config.getConfig("GlobalDBSDLS")
+    except StandardError, ex:
+        msg = "Error reading configuration for GlobalDBSDLS:\n"
+        msg += str(ex)
+        logging.error(msg)
+        raise RuntimeError, msg
+
+    return dbsConfig.get("DBSURL", None)
+
+
+
 class NodeFinder:
     def __init__(self, name):
         self.name = name
@@ -85,13 +107,12 @@ class ResultsFeeder(PluginInterface):
         """
 
         lfnPrefix = '/store/results'
-        dataTier  = 'USER'
 
         # Temporary stuff to override stageout and location
         srmPrefix = 'srm://cmssrm.fnal.gov:8443/srm/managerv2?SFN=/11/'
         lfnPrefix = '/store/user/ewv/testresults'
         self.workflowName = "SR-%s-%s-%s" % \
-            (self.cmsswRelease, self.primaryDataset, self.processedDataset)
+            (self.cmsswRelease, self.primaryDataset, self.outputDataset)
         self.workflowFile = os.path.join(self.workingDir,
                                          '%s.xml' % self.workflowName)
 
@@ -103,14 +124,14 @@ class ResultsFeeder(PluginInterface):
         self.workflow = WorkflowSpec()
         self.workflow.setWorkflowName(self.workflowName)
         self.workflow.parameters["WorkflowType"] = "Merge"
-        self.workflow.parameters["DataTier"] = dataTier
+        self.workflow.parameters["DataTier"] = self.dataTier
         self.workflow.parameters['DBSURL'] = self.dbsUrl
 
         self.inputDatasetName = self.workflow.payload.addInputDataset(
             self.primaryDataset, self.processedDataset
             )
         self.inputDatasetName.update({
-            "DataTier" : dataTier,
+            "DataTier" : self.dataTier,
             })
 
         # Get info from input dataset
@@ -128,6 +149,15 @@ class ResultsFeeder(PluginInterface):
         dstURL = self.dbsUrl
         srcURL = self.inputDBSURL
         path = "/%s/%s/USER" % (self.primaryDataset, self.processedDataset)
+        logging.info("Migrating dataset %s from %s to %s" %
+                     (path, srcURL, dstURL))
+        writer.dbs.migrateDatasetContents(srcURL, dstURL, path,
+                                          '', False, True)
+
+        # Migrate dataset from User's LocalDBS to Global DBS
+
+        writer = DBSWriter(self.globalDbsUrl)
+        dstURL = self.globalDbsUrl
         logging.info("Migrating dataset %s from %s to %s" %
                      (path, srcURL, dstURL))
         writer.dbs.migrateDatasetContents(srcURL, dstURL, path,
@@ -157,7 +187,7 @@ class ResultsFeeder(PluginInterface):
         outputDataset = self.cmsRunNode.addOutputDataset(self.primaryDataset,
                                                          self.outputDataset,
                                                          "Merged")
-        outputDataset["DataTier"] = dataTier
+        outputDataset["DataTier"] = self.dataTier
         outputDataset["NoMerge"] = "True"
         outputDataset["ApplicationName"] = "cmsRun"
         outputDataset["ApplicationProject"] = "CMSSW"
@@ -168,7 +198,7 @@ class ResultsFeeder(PluginInterface):
         outputDataset["PrimaryDatasetType"] = self.dataType
         outputDataset["ParentDataset"] = "/%s/%s/%s" % (self.primaryDataset,
                                                         self.processedDataset,
-                                                        dataTier)
+                                                        self.dataTier)
 
         # Create and populate config description
 
@@ -216,6 +246,8 @@ class ResultsFeeder(PluginInterface):
 
         logging.debug('User loaded params: %s' % userParams)
 
+        self.dataTier  = 'USER'
+
         try:
             self.primaryDataset   = userParams['primaryDataset']
             self.processedDataset = userParams['processedDataset']
@@ -226,8 +258,8 @@ class ResultsFeeder(PluginInterface):
         except KeyError:
             raise RuntimeError("Some parameters missing")
 
-        self.dbsUrl = getInputDBSURL()
-
+        self.dbsUrl       = getInputDBSURL()
+        self.globalDbsUrl = getGlobalDBSURL()
 
     def handleInput(self, payload):
         """
