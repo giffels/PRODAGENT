d5ab4c48c0fcac2757d04455d64422bcd74bada2
hernan <>
2009-07-10 09:18:11 +0000

    allow also single block injection

--- util/PhEDExInjectMergeRegistered.py
+++ util/PhEDExInjectMergeRegistered.py
@@ -5,8 +5,8 @@
 """
 import sys,os,getopt,time
 
-usage = "\n Usage: python PhEDExInjectMergeRegistered.py <options> \n Options: \n --datasetPath=</primarydataset/datatier/processeddataset> \n --help \t\t\t\t print this help \n"
-valid = [ 'datasetPath=' , 'help']
+usage = "\n Usage: python PhEDExInjectMergeRegistered.py <options> \n Options: \n --datasetPath=</primarydataset/datatier/processeddataset> \n --block=</primarydataset/datatier/processeddataset#blockhash> \n --help \t\t\t\t print this help \n"
+valid = [ 'datasetPath=' , 'block=' , 'help']
 try:
     opts, args = getopt.getopt(sys.argv[1:], "", valid)
 except getopt.GetoptError, ex:
@@ -15,22 +15,28 @@ except getopt.GetoptError, ex:
     sys.exit(1)
 
 datasetpath = None
+block = None
 for opt, arg in opts:
     if opt == "--datasetPath":
         datasetpath = arg
+    if opt == "--block":
+        block = arg
     if opt == "--help":
         print usage
         sys.exit(1)
-if datasetpath == None:
-    print "--datasetPath option not provided. For example : --datasetPath /primarydataset/processeddataset/datatier"
+if datasetpath == None and block == None:
+    print "\n Neither --datasetPath nor --block option provided."
     print usage
     sys.exit(1)
 
 
 from MessageService.MessageService import MessageService 
 ms = MessageService()
-
-print "publising Event: PhEDExInjectDataset Payload: %s"%datasetpath
 ms.registerAs("Test")
-ms.publish("PhEDExInjectDataset",datasetpath)
+if datasetpath != None:
+    print "publising Event: PhEDExInjectDataset Payload: %s"%datasetpath
+    ms.publish("PhEDExInjectDataset",datasetpath)
+elif block != None:
+    print "publising Event: PhEDExInjectBlock Payload: %s"%block
+    ms.publish("PhEDExInjectBlock",block)
 ms.commit()
