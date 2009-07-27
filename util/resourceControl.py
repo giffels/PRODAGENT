a3f482f3740d98c9fa67d3502371540e1b78b494
swakef <>
2009-07-27 16:01:16 +0000

    Restore ability for ce to be optional.

--- util/resourceControl.py
+++ util/resourceControl.py
@@ -209,8 +209,9 @@ def newMode():
         raise RuntimeError, msg
 
     if ceName == None:
-        msg = "--ce-name option not provided"
-        raise RuntimeError, msg
+        msg = "--ce-name option not provided. Warning, this is not supported"
+        msg += " by all ResourceMonitor / Submitter combinations\n"
+        print(msg)
     if seName == None:
         msg = "--se-name option not provided"
         raise RuntimeError, msg
