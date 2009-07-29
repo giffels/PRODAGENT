defe9590db094305de5210ffe9b527e8f04acace
dmason <>
2009-07-29 19:46:56 +0000

    adding the patches applied on top of 0_12_15_patch1

--- ops/setupscripts/control/prodagent_install.sh
+++ ops/setupscripts/control/prodagent_install.sh
@@ -12,6 +12,19 @@ cvs co -r PRODAGENT_$PAVERSION PRODAGENT
 # Place any PA patches here -- as in 
 # cvs update -r 1.8 PRODAGENT/src/python/DQMInjector/Plugins/T0ASTPlugin.py
 
+cvs update -r 1.8  PRODAGENT/util/createTier0ProductionWorkflow.py
+cvs update -r 1.11 PRODAGENT/util/resourceControl.py
+cvs update -r 1.5  PRODAGENT/util/jobQueue.py
+cvs update -r 1.19 PRODAGENT/src/python/JobCreator/RunResTools.py
+cvs update -r 1.13 PRODAGENT/src/python/ResourceMonitor/Monitors/T0LSFMonitor.py
+cvs update -r 1.3  PRODAGENT/src/python/ResourceMonitor/Monitors/GlideinWMSMonitor.py
+cvs update -r 1.10 PRODAGENT/src/python/DQMInjector/DQMInjectorComponent.py
+cvs update -r 1.4  PRODAGENT/src/python/JobCreator/RuntimeTools/RuntimeOfflineDQMSetup.py
+cvs update -r 1.11 PRODAGENT/src/python/ErrorHandler/Handlers/ProcessingRunFailureHandler.py 
+cvs update -r 1.9  PRODAGENT/src/python/ErrorHandler/Handlers/ProcessingSubmitFailureHandler.py
+
+echo PA CVS updates done...
+sleep 5
 
 cd PRODAGENT
 source $PBIN/prodagent_init.sh
@@ -31,6 +44,8 @@ cvs co -r PRODCOMMON_$PCVERSION PRODCOMMON
 cvs update -r 1.17 PRODCOMMON/src/python/ProdCommon/FwkJobRep/TaskState.py
 cvs update -r 1.12 PRODCOMMON/src/python/ProdCommon/MCPayloads/DatasetTools.py
 
+echo PC CVS updates done...
+sleep 5
 cd PRODCOMMON
 make
 
