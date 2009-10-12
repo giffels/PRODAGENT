d5636626499553d56bf0e7421dd1138f7ae0b198
giffels <>
2009-10-12 08:45:04 +0000

    Bug fixes

--- src/python/StoreResultsAccountant/RequestQuery.py
+++ src/python/StoreResultsAccountant/RequestQuery.py
@@ -11,6 +11,7 @@ providing information for the bookeeping database
 import os
 import re
 import logging
+import string
 
 from mechanize import Browser
 import simplejson as json
@@ -29,32 +30,27 @@ class RequestQuery:
 
     def closeRequest(self,task):
         if self.isLoggedIn:
-            self.selectQueryForm()
+            response = self.br.open('https://savannah.cern.ch/task/?'+str(task))
 
-            for link in self.br.links(text_regex="#[0-9]+"):
-            ## remove leading &nbsp and # from name
-                id = link.text.replace('#','').decode('utf-8').strip()
+            html = response.read()
 
-                if int(id)==int(task):
-                    response = self.br.follow_link(link)
-
-                    self.br.select_form(name="item_form")
+            self.br.select_form(name="item_form")
                     
-                    control = self.br.find_control("status_id",type="select")
-                    TicketStatusByLabelDict = self.getValueByLabelDict(control)
+            control = self.br.find_control("status_id",type="select")
+            TicketStatusByLabelDict = self.getValueByLabelDict(control)
                 
-                    control.value = [TicketStatusByLabelDict["Closed"]]
+            control.value = [TicketStatusByLabelDict["Closed"]]
 
-                    self.br.submit()
-                    return
+            self.br.submit()
+            
         return
                 
     def createValueDicts(self):       
         if self.isLoggedIn:
             self.br.select_form(name="bug_form")
             
-            control = self.br.find_control("custom_sb1",type="select")
-            self.SiteByValueDict = self.getLabelByValueDict(control)
+            #control = self.br.find_control("custom_sb1",type="select")
+            #self.SiteByValueDict = self.getLabelByValueDict(control)
             control = self.br.find_control("custom_sb2",type="select")
             self.ReleaseByValueDict = self.getLabelByValueDict(control)
             control = self.br.find_control("custom_sb3",type="select")
@@ -73,11 +69,11 @@ class RequestQuery:
                 
         return d
 
-    def getRequests(self):
+    def getRequests(self,**kargs):
         requests = []
         
         if self.isLoggedIn:
-            self.selectQueryForm()
+            self.selectQueryForm(**kargs)
             self.createValueDicts()
         
             self.br.select_form(name="bug_form")
@@ -86,91 +82,97 @@ class RequestQuery:
             html_ouput = response.read()
 
             for link in self.br.links(text_regex="#[0-9]+"):
-                response = self.br.follow_link(link)
+                    response = self.br.follow_link(link)
     
-                ## Get Information
-                self.br.select_form(name="item_form")
+                    ## Get Information
+                    self.br.select_form(name="item_form")
 
-                ## Get old dataset name
-                control = self.br.find_control("custom_tf1",type="text")
-                old_dataset = control.value.split('/')
+                    ## Get old dataset name
+                    control = self.br.find_control("custom_tf1",type="text")
+                    old_dataset = control.value.split('/')
 
-                ## Get DBS URL
-                control = self.br.find_control("custom_tf4",type="text")
-                dbs_url = control.value
+                    ## Get DBS URL
+                    control = self.br.find_control("custom_tf4",type="text")
+                    dbs_url = control.value
 
-                ## Get Site
-                #control = self.br.find_control("custom_sb1",type="select")
-                #site_id =  control.value
+                    ## Get Site
+                    #control = self.br.find_control("custom_sb1",type="select")
+                    #site_id =  control.value
 
-                ## Get Release
-                control = self.br.find_control("custom_sb2",type="select")
-                release_id = control.value
+                    ## Get Release
+                    control = self.br.find_control("custom_sb2",type="select")
+                    release_id = control.value
 
-                ## Get Physics Group
-                control = self.br.find_control("custom_sb3",type="select")
-                group_id = control.value
-                group_squad = 'cms-storeresults-'+self.GroupByValueDict[group_id[0]].replace("-","_")
+                    ## Get Physics Group
+                    control = self.br.find_control("custom_sb3",type="select")
+                    group_id = control.value
+                    group_squad = 'cms-storeresults-'+self.GroupByValueDict[group_id[0]].replace("-","_")
     
-                ## Get current status
-                control = self.br.find_control("resolution_id",type="select")
-                status_id = control.value
+                    ## Get current status
+                    control = self.br.find_control("resolution_id",type="select")
+                    status_id = control.value
                 
-                ## Get current request status
-                control = self.br.find_control("status_id",type="select")
-                request_status_id = control.value
-                RequestStatusByValueDict = self.getLabelByValueDict(control)
+                    ## Get current request status
+                    control = self.br.find_control("status_id",type="select")
+                    request_status_id = control.value
+                    RequestStatusByValueDict = self.getLabelByValueDict(control)
 
-                ## Get assigned to
-                control = self.br.find_control("assigned_to",type="select")
-                AssignedToByValueDict = self.getLabelByValueDict(control)
-                assignedTo_id = control.value
+                    ## Get assigned to
+                    control = self.br.find_control("assigned_to",type="select")
+                    AssignedToByValueDict = self.getLabelByValueDict(control)
+                    assignedTo_id = control.value
 
-                ##Assign task to the physics group squad
-                if AssignedToByValueDict[assignedTo_id[0]]!=group_squad:
-                    control.value = [self.getValueByLabelDict(control)[group_squad]]
-                    self.br.submit()
+                    ##Assign task to the physics group squad
+                    if AssignedToByValueDict[assignedTo_id[0]]!=group_squad:
+                        control.value = [self.getValueByLabelDict(control)[group_squad]]
+                        self.br.submit()
 
-                ## Get new dataset name
-                ##control = self.br.find_control("custom_tf2",type="text")
-                ## remove leading hypernews name and add physics group name
-                stripped_dataset = old_dataset[2].split("-")[1:]
-                #new_dataset = self.GroupByValueDict[group_id[0]]+'-'+''.join(stripped_dataset)
-                new_dataset = 'StoreResults-'+''.join(stripped_dataset)
+                    ## Get new dataset name
+                    ##control = self.br.find_control("custom_tf2",type="text")
+                    ## remove leading hypernews name and add physics group name
+
+                    new_datatset = ""
+
+                    if old_dataset[2].find(self.GroupByValueDict[group_id[0]])==0:
+                        new_dataset = old_dataset[2].replace(self.GroupByValueDict[group_id[0]],"StoreResults",1)
+                    else:
+                        stripped_dataset = old_dataset[2].split("-")[1:]
+                        new_dataset = 'StoreResults-'+'-'.join(stripped_dataset)
                 
-                self.br.back()
+                    self.br.back()
 
-                ## remove leading &nbsp and # from name
-                task = link.text.replace('#','').decode('utf-8').strip()
+                    ## remove leading &nbsp and # from name
+                    task = link.text.replace('#','').decode('utf-8').strip()
 
-                infoDict = {}
+                    infoDict = {}
                 
-                infoDict["primaryDataset"] = old_dataset[1]
-                infoDict["processedDataset"] = old_dataset[2]
-                infoDict["outputDataset"] = new_dataset
-                infoDict["physicsGroup"] = self.GroupByValueDict[group_id[0]]
-                infoDict["inputDBSURL"] = dbs_url
-                infoDict["cmsswRelease"] = self.ReleaseByValueDict[release_id[0]]
-                #infoDict["destinationSite"] = self.SiteByValueDict[site_id[0]]
+                    infoDict["primaryDataset"] = old_dataset[1].replace(' ','')
+                    infoDict["processedDataset"] = old_dataset[2].replace(' ','')
+                    infoDict["outputDataset"] = new_dataset.replace(' ','')
+                    infoDict["physicsGroup"] = self.GroupByValueDict[group_id[0]]
+                    infoDict["inputDBSURL"] = dbs_url.replace(' ','')
+                    infoDict["cmsswRelease"] = self.ReleaseByValueDict[release_id[0]]
+                    #infoDict["destinationSite"] = self.SiteByValueDict[site_id[0]]
 
-                ##Fill json file, if status is done
-                if self.StatusByValueDict[status_id[0]]=='Done':
-                    self.writeJSONFile('Ticket_'+task+'.json', infoDict)
+                    ##Fill json file, if status is done
+                    if self.StatusByValueDict[status_id[0]]=='Done' and RequestStatusByValueDict[request_status_id[0]] != "Closed":
+                        self.writeJSONFile('Ticket_'+task+'.json', infoDict)
+                        
+                    ##Not part of the bookeeping database
+                    #del infoDict["destinationSite"]
 
-                ##Not part of the bookeeping database
-                #del infoDict["destinationSite"]
+                    infoDict["task"] = int(task)
+                    infoDict["ticketStatus"] = self.StatusByValueDict[status_id[0]]
+                    infoDict["assignedTo"] = AssignedToByValueDict[assignedTo_id[0]]
 
-                infoDict["task"] = int(task)
-                infoDict["ticketStatus"] = self.StatusByValueDict[status_id[0]]
-                infoDict["assignedTo"] = AssignedToByValueDict[assignedTo_id[0]]
+                    if infoDict["ticketStatus"] == "Done" and RequestStatusByValueDict[request_status_id[0]] == "Closed":
+                        infoDict["ticketStatus"] = "Closed"
 
-                if infoDict["ticketStatus"] == "Done" and RequestStatusByValueDict[request_status_id[0]] == "Closed":
-                    infoDict["ticketStatus"] == "Closed"
-                
-                requests.append(infoDict)
-    
-            return requests
+                    requests.append(infoDict)
+                    
+        return requests
 
+            
 
     def getValueByLabelDict(self, control):
         d = {}
@@ -207,7 +209,7 @@ class RequestQuery:
         else:
             return True
 
-    def selectQueryForm(self):       
+    def selectQueryForm(self,**kargs):       
         if self.isLoggedIn:
             self.br.select_form(name="bug_form")
 
@@ -217,7 +219,31 @@ class RequestQuery:
             for item in control.items:
                 if item.attrs['label'] == "Test":
                     control.value = [item.attrs['value']]
-            self.br.submit()
+                    
+            ##select number of entries displayed per page
+            control = self.br.find_control("chunksz",type="text")
+            control.value = "150"
+
+            ##check additional searching parameter
+            for arg in kargs:
+                if arg == "approval_status":
+                    #temp = "%s, %s" % (arg,kargs[arg])
+                    #logging.info(temp)
+                    control = self.br.find_control("resolution_id",type="select")
+                    for item in control.items:
+                        if item.attrs['label'] == kargs[arg].strip():
+                            control.value = [item.attrs['value']]
+
+                elif arg == "task_status":
+                    #temp = "%s, %s" % (arg,kargs[arg])
+                    #logging.info(temp)
+                    control = self.br.find_control("status_id",type="select")
+                    for item in control.items:
+                        if item.attrs['label'] == kargs[arg].strip():
+                            control.value = [item.attrs['value']]
+
+            response = self.br.submit()
+            response.read()
 
         return
      
