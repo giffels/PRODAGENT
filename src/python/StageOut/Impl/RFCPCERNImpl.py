08c49d5e1ed705f20111e7b7dd989067914eb55e
hufnagel <>
2009-11-07 17:46:08 +0000

    set fileclass to cms_raw for all RAW data stageouts, cms_production for all non-RAW data stageouts fileclass is only set if /store/data/acqu_era/primds/tier/ is newly created code is only active if the service class is t0export (deadlocks on disk-only pools !)

--- src/python/StageOut/Impl/RFCPCERNImpl.py
+++ src/python/StageOut/Impl/RFCPCERNImpl.py
@@ -49,92 +49,113 @@ class RFCPCERNImpl(StageOutImpl):
         """
 
         # check how the targetPFN looks like and parse out the target dir
-        targetdir = None
+        targetDir = None
 
-        if targetdir == None:
+        if targetDir == None:
             regExpParser = re.compile('/+castor/(.*)')
             match = regExpParser.match(targetPFN)
             if ( match != None ):
-                targetdir = os.path.dirname(targetPFN)
+                targetDir = os.path.dirname(targetPFN)
 
-        if targetdir == None:
+        if targetDir == None:
             regExpParser = re.compile('rfio:/+castor/(.*)')
             match = regExpParser.match(targetPFN)
             if ( match != None ):
-                targetdir = os.path.dirname('/castor/' + match.group(1))
+                targetDir = os.path.dirname('/castor/' + match.group(1))
 
-        if targetdir == None:
+        if targetDir == None:
             regExpParser = re.compile('rfio:.*path=/+castor/(.*)')
             match = regExpParser.match(targetPFN)
             if ( match != None ):
-                targetdir = os.path.dirname('/castor/' + match.group(1))
+                targetDir = os.path.dirname('/castor/' + match.group(1))
 
         # raise exception if we have no rule that can parse the target dir
-        if targetdir == None:
+        if targetDir == None:
             raise StageOutError("Cannot parse directory out of targetPFN")
 
         # remove multi-slashes from path
-        while ( targetdir.find('//') > -1 ):
-            targetdir = targetdir.replace('//','/')
+        while ( targetDir.find('//') > -1 ):
+            targetDir = targetDir.replace('//','/')
 
-        #
-        # determine file class from LFN
-        #
-        fileclass = None
+        print "DEBUG 111 before targetDir check"
 
-        # temp or unmerged files use cms_no_tape
-        if ( fileclass == None ):
-            regExpParser = re.compile('.*/castor/cern.ch/cms/store/temp/')
-            if ( regExpParser.match(targetdir) != None ):
-                fileclass = 'cms_no_tape'
-        if ( fileclass == None ):
-            regExpParser = re.compile('.*/castor/cern.ch/cms/store/unmerged/')
-            if ( regExpParser.match(targetdir) != None ):
-                fileclass = 'cms_no_tape'
-
-        # RAW data files use cms_raw
-        if ( fileclass == None ):
-            regExpParser = re.compile('.*/castor/cern.ch/cms/store/data/[^/]+/[^/]+/RAW/')
-            if ( regExpParser.match(targetdir) != None ):
-                fileclass = 'cms_raw'
-
-        # otherwise we assume another type of production file
-        if ( fileclass == None ):
-            fileclass = 'cms_production'
-
-        print "Setting fileclass to : %s" % fileclass
-
-        # check if directory exists
-        rfstatCmd = "rfstat %s 2> /dev/null | grep Protection" % targetdir
-        print "Check dir existence : %s" % rfstatCmd
+        # check if targetDir exists
+        targetDirCheck = "rfstat %s 2> /dev/null | grep Protection" % targetDir
+        print "Check dir existence : %s" % targetDirCheck
         try:
-            rfstatExitCode, rfstatOutput = runCommandWithOutput(rfstatCmd)
+            targetDirCheckExitCode, targetDirCheckOutput = runCommandWithOutput(targetDirCheck)
         except Exception, ex:
             msg = "Error: Exception while invoking command:\n"
-            msg += "%s\n" % rfstatCmd
+            msg += "%s\n" % targetDirCheck
             msg += "Exception: %s\n" % str(ex)
             msg += "Fatal error, abort stageout..."
             raise StageOutError(msg)
 
         # does not exist => create it
-        if rfstatExitCode:
-            if ( fileclass != None ):
-                self.createDir(targetdir, '000')
-                self.setFileClass(targetdir,fileclass)
-                self.changeDirMode(targetdir, self.permissions)
-            else:
-                self.createDir(targetdir, self.permissions)
+        if targetDirCheckExitCode:
+
+            print "DEBUG 222 have to create targetDir"
+
+            # only use the fileclass code path if we run on t0export
+            serviceClass = os.environ.get('STAGE_SVCCLASS', None)
+            if serviceClass == 't0export':
+
+                # determine file class from PFN
+                fileclass = None
+
+                print "DEBUG 333 checking PFN for fileclass override"
+
+                # check for correct naming convention in PFN
+                #regExpParser = re.compile('/castor/cern.ch/cms/store/data/[^/]+/[^/]+/[^/]+/')
+                regExpParser = re.compile('/castor/cern.ch/cms/T0/hufnagel/recotest/store/data/([^/]+)/([^/]+)/([^/]+)/')
+                match = regExpParser.match(targetDir)
+                if ( match != None ):
+
+                    # RAW data files use cms_raw, all others cms_production
+                    if match.group(3) == 'RAW':
+                        fileclass = 'cms_raw'
+                    else:
+                        fileclass = 'cms_production'
+
+                    fileclassDir = '/castor/cern.ch/cms/T0/hufnagel/recotest/store/data/%s/%s/%s' % match.group(1,2,3)
+
+                    print "DEBUG 444 want fileclass %s on %s" % (fileclass, fileclassDir)
+
+                    # check fileclassDir existance
+                    fileclassDirCheck = "rfstat %s 2> /dev/null | grep Protection" % fileclassDir
+                    print "Check dir existence : %s" % fileclassDirCheck
+                    try:
+                        fileclassDirCheckExitCode, fileclassDirCheckOutput = runCommandWithOutput(fileclassDirCheck)
+                    except Exception, ex:
+                        msg = "Error: Exception while invoking command:\n"
+                        msg += "%s\n" % rfstatCmd
+                        msg += "Exception: %s\n" % str(ex)
+                        msg += "Fatal error, abort stageout..."
+                        raise StageOutError(msg)
+
+                    # does not exist => create it
+                    if fileclassDirCheckExitCode:
+                        print "DEBUG 555 creating %s" % fileclassDir
+                        self.createDir(fileclassDir, self.permissions)
+                        if ( fileclass != None ):
+                            print "DEBUG 666 setting fileclass %s" % fileclass
+                            self.setFileClass(fileclassDir,fileclass)
+                    else:
+                        # check if this is a directory
+                        regExpParser = re.compile('Protection.*: d')
+                        if ( regExpParser.match(fileclassDirCheckOutput) == None):
+                            raise StageOutError("Output path is not a directory !")
+
+            # now create targetDir
+            print "DEBUG 777 setting fileclass %s" % fileclass
+            self.createDir(targetDir, self.permissions)
+
         else:
+
             # check if this is a directory
             regExpParser = re.compile('Protection.*: d')
-            if ( regExpParser.match(rfstatOutput) == None):
+            if ( regExpParser.match(targetDirCheckOutput) == None):
                 raise StageOutError("Output path is not a directory !")
-            else:
-                # check if directory is writable
-                regExpParser = re.compile('Protection.*: d---------')
-                if ( regExpParser.match(rfstatOutput) != None and fileclass != None ):
-                    self.setFileClass(targetdir,fileclass)
-                    self.makeDirWritable(targetdir)
 
         return
 
@@ -149,16 +170,15 @@ class RFCPCERNImpl(StageOutImpl):
         result = "rfcp "
         if options != None:
             result += " %s " % options
-        result += " '%s' " % sourcePFN
-        result += " '%s' " % targetPFN
+        result += " \"%s\" " % sourcePFN
+        result += " \"%s\" " % targetPFN
         
         if self.stageIn:
             remotePFN, localPFN = sourcePFN, targetPFN
         else:
             remotePFN, localPFN = targetPFN, sourcePFN
         
-        result += "\nFILE_SIZE=`stat -c %s"
-        result += " %s `;\n" % localPFN
+        result += "\nFILE_SIZE=`rfstat \"%s\" | grep Size | cut -f2 -d:`\n" % localPFN
         result += " echo \"Local File Size is: $FILE_SIZE\"; DEST_SIZE=`rfstat '%s' | grep Size | cut -f2 -d:` ; if [ $DEST_SIZE ] && [ $FILE_SIZE == $DEST_SIZE ]; then exit 0; else echo \"Error: Size Mismatch between local and SE\"; exit 60311 ; fi " % remotePFN
 
         return result
@@ -168,12 +188,10 @@ class RFCPCERNImpl(StageOutImpl):
         """
         _removeFile_
 
-        CleanUp pfn provided: specific for Castor-1
-
         """
-        command = "stager_rm -M %s ; nsrm %s" % (pfnToRemove, pfnToRemove)
+        command = "stager_rm -M \"%s\" ; nsrm \"%s\"" % (pfnToRemove, pfnToRemove)
         execute(command)
-
+        return
 
     def createDir(self, directory, mode):
         """
@@ -182,9 +200,8 @@ class RFCPCERNImpl(StageOutImpl):
         Creates directory with no permissions
 
         """
-        cmd = "nsmkdir -m %s -p %s" % (mode, directory)
+        cmd = "nsmkdir -m %s -p \"%s\"" % (mode, directory)
         execute(cmd)
-
         return
 
 
@@ -195,20 +212,7 @@ class RFCPCERNImpl(StageOutImpl):
         Sets fileclass for specified directory
 
         """
-        cmd = "nschclass %s %s" % (fileclass, directory)
-        execute(cmd)
-
-        return
-
-
-    def changeDirMode(self, directory, mode):
-        """
-        _createDir_
-
-        Sets mode for directory
-
-        """
-        cmd = "nschmod %s %s" % (mode, directory)
+        cmd = "nschclass %s \"%s\"" % (fileclass, directory)
         execute(cmd)
 
         return
