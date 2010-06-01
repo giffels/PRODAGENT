c4faf0bd39e002b1d1055505a75c9e2bb625b3b5
edelmann <>
2010-06-01 12:36:43 +0000

    Report job status to dashboard.

--- src/python/CondorTracker/Trackers/ARCTracker.py
+++ src/python/CondorTracker/Trackers/ARCTracker.py
@@ -9,6 +9,7 @@ Tracker for Nordugrid ARC submissions
 
 import logging
 import os
+import time
 
 from CondorTracker.TrackerPlugin import TrackerPlugin
 from CondorTracker.Registry import registerTracker
@@ -16,6 +17,7 @@ import CondorTracker.CondorTrackerDB as TrackerDB
 from ProdAgent.WorkflowEntities import Job
 import ProdCommon.FwkJobRep.ReportState as ReportState
 from ProdAgent.Resources import ARC
+from ShREEK.CMSPlugins.DashboardInfo import DashboardInfo
 
 
 
@@ -108,36 +110,50 @@ class ARCTracker(TrackerPlugin):
 
         """
         logging.info("ARC: Submitted Count: %s" % len(submitted))
-        for subId in submitted:
-            status = self.jobs.get(subId, None)
+        toDashboard = {}
+        for jid in submitted:
+            status = self.jobs.get(jid, None)
   
             # Job not in getJobStatus() output, check job report
             if status == None:
-                msg = "No Status entry for %s, checking job report" % subId
+                msg = "No Status entry for %s, checking job report" % jid
                 logging.debug(msg)
-                status = self.jobReportStatus(subId)
+                status = self.jobReportStatus(jid)
                 
             # If status still None, declare job lost/failed
             if status == None:
-                self.TrackerDB.jobFailed(subId)
-                logging.debug("Job %s has been lost" % (subId))
+                self.TrackerDB.jobFailed(jid)
+                logging.debug("Job %s has been lost" % (jid))
+                toDashboard['StatusValue'] = 'Aborted'
+                self.publishStatusToDashboard(jid, toDashboard)
                 continue
 
             # Now examine the status value, not sure what these are, but act
             # accordingly
             if status == "PEND":
                 # Still submitted, nothing to be done
-                logging.debug("Job %s is pending" % (subId))
-                continue 
-            if status in ("RUN", "DONE"):
-                # Is running or completed already, forward to running handler
-                self.TrackerDB.jobRunning(subId)
-                logging.debug("Job %s is running or finished" % (subId))
-                continue
-            if status == "EXIT":
+                logging.debug("Job %s is pending" % (jid))
+                toDashboard['StatusValue'] = 'Submitted'
+                self.publishStatusToDashboard(jid, toDashboard)
+            elif status == "RUN":
+                self.TrackerDB.jobRunning(jid)
+                logging.debug("Job %s is running" % (jid))
+                toDashboard['StatusValue'] = 'Running'
+                self.publishStatusToDashboard(jid, toDashboard)
+            elif status == "DONE":
+                # Let's forward the handling of finished jobs to
+                # 'updateRunning'.
+                self.TrackerDB.jobRunning(jid)
+                logging.debug("Job %s is finished" % (jid))
+            elif status == "EXIT":
                 # Failed or Aborted
-                logging.debug("Job %s is held..." % (subId))
-                self.TrackerDB.jobFailed(subId)
+                logging.debug("Job %s is held..." % (jid))
+                self.TrackerDB.jobFailed(jid)
+                toDashboard['StatusValue'] = 'Aborted'
+                self.publishStatusToDashboard(jid, toDashboard)
+            else:
+                # I don't think this is supposed to be possible happen!
+                logging.warning("WTF? Job %s has status %s" % (jid, status))
                 
         return
 
@@ -150,35 +166,40 @@ class ARCTracker(TrackerPlugin):
 
         """
         logging.info("ARC: Running Count: %s" % len(running))
-        for id in running:
+        toDashboard = {}
+        for jid in running:
 
-            status = self.jobs.get(id, None)
+            status = self.jobs.get(jid, None)
   
             # Job not in getJobStatus output, check job report
             if status == None:
-                msg = "No Status entry for %s, checking job report" % id
+                msg = "No Status entry for %s, checking job report" % jid
                 logging.debug(msg)
-                status = self.jobReportStatus(id)
+                status = self.jobReportStatus(jid)
                 
             # If status still None, declare job lost/failed
             if status == None:
-                self.TrackerDB.jobFailed(id)
-                logging.debug("Job %s has been lost" % (id))
-                continue
-            
-            if status == "RUN":
-                # Is running
-                logging.debug("Job %s is still running" % (id))
-                continue
-            if status == "DONE":
-                # Is Complete 
-                self.TrackerDB.jobComplete(id)
-                report = self.findJobReport(id, True)
-                logging.debug("Job %s complete with report %s" % (id,report))
-                continue
-            if status == "EXIT":
-                logging.debug("Job %s is held..." % (id))
-                self.TrackerDB.jobFailed(id)
+                self.TrackerDB.jobFailed(jid)
+                logging.debug("Job %s has been lost" % (jid))
+                toDashboard['StatusValue'] = 'Aborted'
+                self.publishStatusToDashboard(jid, toDashboard)
+            elif status == "RUN":
+                logging.debug("Job %s is still running" % (jid))
+                toDashboard['StatusValue'] = 'Running'
+                self.publishStatusToDashboard(jid, toDashboard)
+            elif status == "DONE":
+                self.TrackerDB.jobComplete(jid)
+                report = self.findJobReport(jid, True)
+                logging.debug("Job %s complete with report %s" % (jid,report))
+                toDashboard['StatusValue'] = 'Done'
+                self.publishStatusToDashboard(jid, toDashboard)
+            elif status == "EXIT":
+                logging.debug("Job %s is held..." % (jid))
+                self.TrackerDB.jobFailed(jid)
+                toDashboard['StatusValue'] = 'Aborted'
+                self.publishStatusToDashboard(jid, toDashboard)
+            else:
+                logging.warning("WTF? Job %s has status %i" % (jid, status))
 
             
     def updateComplete(self, *complete):
@@ -273,6 +294,117 @@ class ARCTracker(TrackerPlugin):
         if ReportState.checkSuccess(report):
             return ARC.StatusCodes["FINISHED"]
         return ARC.StatusCodes["FAILED"]
+
+
+    def publishStatusToDashboard(self, jobSpecId, data):
+        """
+        _publishStatusToDashboard_
+
+        Publish the dashboard info to the appropriate destination
+
+        """
+        #  //
+        # // Check for dashboard usage
+        #//
+        self.usingDashboard = {'use' : 'True',
+                               'address' : 'cms-pamon.cern.ch',
+                               'port' : 8884}
+        try:
+            submitterConfig = loadPluginConfig("JobSubmitter", "Submitter")
+            dashboardCfg = submitterConfig.get('Dashboard', {})
+            self.usingDashboard['use'] = dashboardCfg.get(
+                "UseDashboard", "False")
+            self.usingDashboard['address'] = dashboardCfg.get(
+                "DestinationHost")
+            self.usingDashboard['port'] = int(dashboardCfg.get(
+                "DestinationPort"))
+            logging.debug("dashboardCfg = " + str(self.usingDashboard) )
+        except:
+            logging.info("No Dashboard section in SubmitterPluginConfig")
+            logging.info("Taking default values:")
+            logging.info("dashboardCfg = " + str(self.usingDashboard))
+
+        if self.usingDashboard['use'].lower().strip() == "false":
+            logging.info("Skipping Dasboard report.")
+            return
+
+        # Instantiate DashboardInfo strcuture
+        dashboardInfo = DashboardInfo()
+
+        # Get DashboardInfo file in jobCache dir
+        jobCache = self.getJobCache(jobSpecId)
+        dashboardInfoFile = os.path.join(jobCache, "DashboardInfo.xml")
+        if not os.path.exists(dashboardInfoFile):
+            msg = "Dashboard Info file not found\n"
+            msg += "%s\n" % dashboardInfoFile
+            msg += "Skipping dashboard report for %s\n" % jobSpecId
+            logging.debug(msg)
+            return
+        try:
+            dashboardInfo.read(dashboardInfoFile)
+        except Exception, msg:
+            logging.error(
+                "Couldn't read dashboardInfoFile for job %s. %s" % (
+                    jobSpecId, str(msg))
+            )
+            msg = "Skipping dashboard report for %s\n" % jobSpecId
+            logging.debug(msg)
+            return
+
+
+        # Fill dashboard info
+        oldStatus = dashboardInfo.get('StatusValue', '')
+        dashboardInfo['StatusValue'] = data.get('StatusValue', '')
+
+        statusReasonMap = {
+            'Aborted': 'Job has been aboorted.',
+            'Submitted': 'Job has been submitted.',
+            'Done': 'Job terminated succesfully.',
+            'Running': 'Job is running.'
+        }
+        if data.get('StatusValueReason', ''):
+            dashboardInfo['StatusValueReason'] = \
+                data.get('StatusValueReason', '')
+        else:
+            dashboardInfo['StatusValueReason'] = statusReasonMap.get(
+                dashboardInfo['StatusValue'], '')
+
+        if data.get('StatusEnterTime', ''):
+            dashboardInfo['StatusEnterTime'] = data.get('StatusEnterTime', '')
+        elif oldStatus != dashboardInfo['StatusValue']:
+            dashboardInfo['StatusEnterTime'] = time.time()
+        if data.get('StatusDestination', ''):
+            if data['StatusDestination'].lower().find('unknown') == -1:
+                dashboardInfo['StatusDestination'] = data['StatusDestination']
+
+        # Broadcasting data
+        try:
+            dashboardInfo.publish(1)
+            logging.debug("dashboard info sent for job %s" % jobSpecId)
+        except Exception, msg:
+            logging.error(
+                "Cannot publish dashboard information for job %s. %s" % (
+                    jobSpecId, str(msg))
+            )
+
+        # update DasboardInfo file
+        logging.debug("Creating dashboardInfoFile %s." % dashboardInfoFile)
+        try:
+            dashboardInfo.write(dashboardInfoFile)
+        except Exception, msg:
+            logging.error(
+                "Couldn't create dashboardInfoFile for job %s. %s" % (
+                    jobSpecId, str(msg))
+            )
+
+        logging.debug("Information published in Dashboard:")
+        msg = "\n - task: %s\n - job: %s" % (dashboardInfo.task,
+            dashboardInfo.job)
+        for key, value in dashboardInfo.items():
+            msg += "\n - %s: %s" % (key, value)
+        logging.debug(msg)
+
+        return
 
 
 registerTracker(ARCTracker, ARCTracker.__name__)
