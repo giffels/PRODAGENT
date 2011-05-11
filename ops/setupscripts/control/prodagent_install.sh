3d47eec652e78ff19277532d942c3a44a90b4bbf
samir <>
2011-05-11 14:56:19 +0000

    adding extra patches to pre3

--- ops/setupscripts/control/prodagent_install.sh
+++ ops/setupscripts/control/prodagent_install.sh
@@ -12,6 +12,7 @@ cvs co -r PRODAGENT_$PAVERSION PRODAGENT
 # Place any PA patches here -- as in 
 cvs upd -r 1.13 PRODAGENT/src/python/StageOut/Impl/RFCPCERNImpl.py 
 cvs upd -r 1.27 PRODAGENT/src/python/JobCreator/RuntimeTools/RuntimeOfflineDQM.py 
+cvs upd -r 1.60 PRODAGENT/src/python/JobCreator/JobCreatorComponent.py 
 
 echo PA CVS updates done...
 sleep 5
@@ -55,6 +56,9 @@ fi
 # T0 specific components
 
 cvs co -r T0_$T0VERSION T0
+
+# T0 Patches here
+cvs upd -r 1.52 T0/src/python/T0/WorkflowFactory/FactoryInterface.py
 
 cvs co -r WMCORE_$WMCOREVERSION WMCORE
  
