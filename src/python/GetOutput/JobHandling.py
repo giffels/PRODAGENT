fd3216bdffb7bd3d6b7eea555146fa2eeb1ab6db
giffels <>
2012-10-18 15:07:48 +0000

    This commit was manufactured by cvs2svn to create tag 'PRODAGENT_0_12_18_CRAB_3'.
    Sprout from master 2010-08-12 14:52:16 UTC spiga 'typo'
    Cherrypick from master 2012-10-18 15:07:47 UTC belforte 'remove broken line :-(':
        src/python/GetOutput/JobHandling.py
        src/python/JobTracking/QueryStatus.py
        src/python/JobTracking/TrackingComponent.py

--- src/python/GetOutput/JobHandling.py
+++ src/python/GetOutput/JobHandling.py
@@ -5,8 +5,8 @@ _JobHandling_
 """
 
 
-__revision__ = "$Id: JobHandling.py,v 1.19 2010/03/01 16:15:00 swakef Exp $"
-__version__ = "$Revision: 1.19 $"
+__revision__ = "$Id: JobHandling.py,v 1.25 2012/10/18 14:48:42 belforte Exp $"
+__version__ = "$Revision: 1.25 $"
 
 import os
 import logging
@@ -121,15 +121,20 @@ class JobHandling:
                           (self.fullId(job), str(success)) )
 
         # FJR missing or corrupt: create one based on db or assume failed
-        except InvalidReport:
+        except InvalidReport, msg:
+            logging.debug("Job %s : no valid FJR found: %s" % \
+                          (self.fullId(job), msg) )
             success = False
-            job.runningJob["applicationReturnCode"] = str(50117)
-            job.runningJob["wrapperReturnCode"] = str(50117)
-            exitCode = 50117
+            if str(msg) == "FrameworkJobReport is missing." :
+                exitCode=50700
+            else :
+                exitCode=50117
+            job.runningJob["applicationReturnCode"] = str(exitCode)
+            job.runningJob["wrapperReturnCode"] = str(exitCode)
 
             # write fake fwjr
-            logging.debug("Job %s : write fake fwjr: %s" % \
-                          (self.fullId(job), str(success)) )
+            logging.debug("Job %s : write fake fwjr with exitCode %s. Success flag is: %s" % \
+                          (self.fullId(job), str(exitCode), str(success)) )
             self.writeFwkJobReport( jobSpecId, exitCode, reportfilename )
 
 
@@ -422,7 +427,12 @@ class JobHandling:
         try:
             logging.debug( 'Job %s REBOUNCE DBG : %s, %s' % \
                            (self.fullId(job), source, dest) )
-            sbi.copy( source, dest, credential )
+#        try not to transfer via gridftp
+            if  os.access (source, os.R_OK) :
+                copy (source, dest)
+            else:
+                sbi.copy( source, dest, credential )
+
         except Exception, e:
             logging.info("Job %s Report rebounce transfer fail : %s " \
                          % ( self.fullId(job), str(e) ) )
--- src/python/JobTracking/TrackingComponent.py
+++ src/python/JobTracking/TrackingComponent.py
@@ -17,8 +17,8 @@ payload of the JobFailure event
 
 """
 
-__revision__ = "$Id: TrackingComponent.py,v 1.69 2010/06/29 08:34:57 direyes Exp $"
-__version__ = "$Revision: 1.69 $"
+__revision__ = "$Id: TrackingComponent.py,v 1.70 2010/08/10 21:58:49 spiga Exp $"
+__version__ = "$Revision: 1.70 $"
 
 import os
 import os.path
@@ -502,14 +502,15 @@ class TrackingComponent:
         if dashboardInfoFile is None or not os.path.exists(dashboardInfoFile):
             task = self.bossLiteSession.loadTask(job['taskId'], deep=False)
             match = str(job.runningJob['schedulerId'])
-            m = re.search("glidein", match)
+            # m = re.search("submit", match)
+            m = re.search("https", match)
 
             dashboardInfo.task = task['name']
             if m:
-               dashboardInfo.job = str(job['jobId']) + '_https://' + \
+               dashboardInfo.job = str(job['jobId']) + '_' + \
                                 job.runningJob['schedulerId']
             else:
-               dashboardInfo.job = str(job['jobId']) + '_' + \
+               dashboardInfo.job = str(job['jobId']) + '_https://' + \
                                 job.runningJob['schedulerId']
             dashboardInfo['JSTool'] = 'crab'
             dashboardInfo['JSToolUI'] = os.environ['HOSTNAME']
